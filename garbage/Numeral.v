76c79fc366dbff5191c66b519c14f31a04aac522
Sampsa Kiiskinen <tuplanolla@gmail.com>
2019-10-24 20:06:53 +0300

    Numeral adventures.

--- garbage/Numeral.v
+++ garbage/Numeral.v
@@ -1,25 +1,38 @@
-From Coq Require Import NArith List Decimal Ascii String.
+From Coq Require Import Ascii Decimal List NArith String.
 
-Import ListNotations.
+Module StringNotations.
+
+Notation "x '::' xs" := (String x xs) : string_scope.
+Notation "'[' ']'" := EmptyString : string_scope.
+Notation "'[' x ']'" := (String x EmptyString) : string_scope.
+Notation "'[' x ';' y ';' .. ';' z ']'" :=
+  (String x (String y .. (String z EmptyString) ..)) : string_scope.
+
+End StringNotations.
+
+Import StringNotations.
 
 Fixpoint string_of_uint (n : uint) : string :=
   match n with
-  | Nil => ""%string
-  | D0 p => String "0"%char (string_of_uint p)
-  | D1 p => String "1"%char (string_of_uint p)
-  | D2 p => String "2"%char (string_of_uint p)
-  | D3 p => String "3"%char (string_of_uint p)
-  | D4 p => String "4"%char (string_of_uint p)
-  | D5 p => String "5"%char (string_of_uint p)
-  | D6 p => String "6"%char (string_of_uint p)
-  | D7 p => String "7"%char (string_of_uint p)
-  | D8 p => String "8"%char (string_of_uint p)
-  | D9 p => String "9"%char (string_of_uint p)
+  | Nil => ""
+  | D0 p => "0" :: string_of_uint p
+  | D1 p => "1" :: string_of_uint p
+  | D2 p => "2" :: string_of_uint p
+  | D3 p => "3" :: string_of_uint p
+  | D4 p => "4" :: string_of_uint p
+  | D5 p => "5" :: string_of_uint p
+  | D6 p => "6" :: string_of_uint p
+  | D7 p => "7" :: string_of_uint p
+  | D8 p => "8" :: string_of_uint p
+  | D9 p => "9" :: string_of_uint p
   end.
 
 Section Natural.
 
-Import N.
+Import ListNotations N.
+
+Definition string_of_N (n : N) : string :=
+  string_of_uint (to_uint n).
 
 Open Scope N_scope.
 
@@ -28,9 +41,6 @@ Fixpoint N_compute (ns : list N) : N :=
   | [] => 0
   | p :: ps => p + N_compute ps
   end.
-
-Definition string_of_N (n : N) : string :=
-  string_of_uint (to_uint n).
 
 Fixpoint N_format (ns : list N) : string :=
   match ns with
@@ -45,17 +55,14 @@ End Natural.
 
 Section Positive.
 
-Import Pos.
+Import ListNotations Pos.
 
 Open Scope positive_scope.
 
-Reserved Notation "'double'" (at level 0, no associativity).
-Notation "'double'" := xO (only parsing) : positive_scope.
-
 Fixpoint positive_powers_of_two (n : positive) : list positive :=
   match n with
-  | p ~ 1 => 1 :: map double (positive_powers_of_two p)
-  | p ~ 0 => map double (positive_powers_of_two p)
+  | p ~ 1 => 1 :: map xO (positive_powers_of_two p)
+  | p ~ 0 => map xO (positive_powers_of_two p)
   | 1 => [1]
   end.
 
@@ -63,16 +70,20 @@ End Positive.
 
 Section Natural.
 
-Import N.
+Import ListNotations N.
 
-Open Scope N_scope.
 Open Scope string_scope.
+Open Scope list_scope.
+Open Scope N_scope.
 
 Definition N_powers_of_two (n : N) : list N :=
   match n with
   | 0 => []
   | pos p => map pos (positive_powers_of_two p)
   end.
+
+Compute map (fun n : N => N_compute (N_powers_of_two n))
+  (map of_nat (seq 0 16)).
 
 Compute map (fun n : N => N_format (N_powers_of_two n))
   (map of_nat (seq 0 16)).
@@ -82,7 +93,7 @@ Lemma N_compute_map_double : forall ns : list N,
 Proof.
   intros ns. induction ns as [| p ps IH].
   - reflexivity.
-  - cbn. rewrite IH. rewrite double_add. reflexivity. Qed.
+  - cbn [map N_compute]. rewrite IH. rewrite double_add. reflexivity. Qed.
 
 Theorem N_powers_of_two_valid : forall n : N,
   N_compute (N_powers_of_two n) = n.
@@ -90,49 +101,39 @@ Proof.
   intros n. destruct n as [| p].
   - reflexivity.
   - cbv [N_powers_of_two]. induction p as [q IH | q IH |].
-    + cbn. rewrite map_map.
+    + cbn [positive_powers_of_two map N_compute]. rewrite map_map.
       replace (fun x : positive => pos x ~ 0) with
-        (fun x : positive => N.double (pos x)) by reflexivity.
+        (fun x : positive => double (pos x)) by reflexivity.
       rewrite <- map_map. rewrite N_compute_map_double.
       rewrite IH. reflexivity.
-    + cbn. rewrite map_map.
+    + cbn [positive_powers_of_two]. rewrite map_map.
       replace (fun x : positive => pos x ~ 0) with
-        (fun x : positive => N.double (pos x)) by reflexivity.
+        (fun x : positive => double (pos x)) by reflexivity.
       rewrite <- map_map. rewrite N_compute_map_double.
       rewrite IH. reflexivity.
     + reflexivity. Qed.
 
 End Natural.
 
-(** Add a power of two and a residue. *)
-
-Section Positive.
-
-Import Pos.
-
-Open Scope positive_scope.
-
-Reserved Notation "x '/' '2'" (at level 40, left associativity).
-Notation "x '/' '2'" := (Pos.div2 x) : positive_scope.
-
-Definition positive_power_and_residue (n : positive) : list positive :=
-  if n =? 1 then [1] else let p : positive := size n - 1 in
-  if n =? 2 ^ p then [n / 2; n / 2] else [n - 2 ^ p; 2 ^ p].
-
-End Positive.
+(** Add a power of two and its residue. *)
 
 Section Natural.
 
-Import N.
+Import ListNotations N.
 
-Open Scope N_scope.
 Open Scope string_scope.
+Open Scope list_scope.
+Open Scope N_scope.
 
 Definition N_power_and_residue (n : N) : list N :=
-  match n with
-  | 0 => []
-  | pos p => map pos (positive_power_and_residue p)
-  end.
+  if n =? 0 then [] else
+  if n =? 1 then [1] else
+  let p : N := log2 n in
+  if n =? 2 ^ p then [n / 2; n / 2] else
+  [n - 2 ^ p; 2 ^ p].
+
+Compute map (fun n : N => N_compute (N_power_and_residue n))
+  (map of_nat (seq 0 16)).
 
 Compute map (fun n : N => N_format (N_power_and_residue n))
   (map of_nat (seq 0 16)).
@@ -140,19 +141,21 @@ Compute map (fun n : N => N_format (N_power_and_residue n))
 Theorem N_power_and_residue_valid : forall n : N,
   N_compute (N_power_and_residue n) = n.
 Proof.
-  intros n. destruct n as [| p].
-  - reflexivity.
-  - cbv [N_power_and_residue positive_power_and_residue].
-    destruct (Pos.eqb_spec p 1)%positive as [H | H].
-    + rewrite H. reflexivity.
-    + destruct (Pos.eqb_spec p (2 ^ (Pos.size p - 1)))%positive as [H' | H'].
-      * cbn. f_equal. destruct p as [q | q |]. all: cbn in *.
-        -- admit.
-        -- rewrite Pos.add_diag. reflexivity.
-        -- exfalso. apply H. reflexivity.
-      * cbn. f_equal. destruct p as [q | q |]. all: cbn in *.
-        -- admit.
-        -- admit.
-        -- exfalso. apply H. reflexivity. Admitted.
+  intros n. cbv [N_power_and_residue].
+  destruct (eqb_spec n 0) as [H0 | H0].
+  - rewrite H0. reflexivity.
+  - destruct (eqb_spec n 1) as [H1 | H1].
+    + rewrite H1. reflexivity.
+    + destruct (eqb_spec n (2 ^ log2 n)) as [H2 | H2].
+      * cbv [N_compute]. ring_simplify.
+        destruct (log2 n) as [| p _] using peano_ind.
+        -- exfalso. apply H1. apply H2.
+        -- pose proof (neq_succ_0 1) as H.
+          symmetry. rewrite (div_exact n 2 H). rewrite (mod_divides n 2 H).
+          exists (2 ^ p). rewrite <- pow_succ_r'. apply H2.
+      * cbv [N_compute]. ring_simplify.
+        rewrite (sub_add (2 ^ log2 n) n).
+        -- reflexivity.
+        -- apply log2_spec. rewrite <- (neq_0_lt_0 n). apply H0. Qed.
 
 End Natural.
--- prototype/Offers/FieldConstants.v
+++ prototype/Offers/FieldConstants.v
@@ -3,19 +3,85 @@ From Maniunfold.Has Require Export
 From Maniunfold.ShouldHave Require Export
   FieldNotations.
 
+(** Semirings only come with zero and one out of the box,
+    so we have to construct all the other natural numbers
+    from repeated additions and multiplications.
+    The optimal way to do this is quite complicated,
+    as demonstrated in OEIS sequence A005245,
+    but we can make the job more manageable
+    by limiting ourselves to just addition. *)
+
+(** The following definitions produce
+    the natural numbers using the simplest suboptimal reduction tree. *)
+
+Section Context.
+
+Context {A : Type} {has_add : HasAdd A} {has_zero : HasZero A}
+  {has_one : HasOne A}.
+
+Fixpoint of_nat (n : nat) : A :=
+  match n with
+  | O => 0
+  | S p => 1 + of_nat p
+  end.
+
+End Context.
+
+(** The following definitions produce
+    the natural numbers using one of the optimal reduction trees. *)
+
+(** TODO Clean up the termination proof. *)
+
+Require Import List Program Recdef Omega PeanoNat.
+
+Section Context.
+
+Import ListNotations Nat.
+
+Context {A : Type} {has_add : HasAdd A} {has_zero : HasZero A}
+  {has_one : HasOne A}.
+
+Function of_nat' (n : nat) {measure id n} : A :=
+  if n =? 0 then 0 else
+  if n =? 1 then 1 else
+  let p : nat := log2 n in
+  if n =? 2 ^ p then of_nat' (n / 2) + of_nat' (n / 2) else
+  of_nat' (n - 2 ^ p) + of_nat' (2 ^ p).
+Proof.
+  { intros n H0 H1 H2.
+    apply eqb_neq in H0. apply eqb_neq in H1. apply eqb_eq in H2. cbv [id].
+    rewrite H2.
+    pose proof (neq_succ_0 1) as H.
+    epose proof log2_spec n _ as H'. rewrite pow_succ_r' in H'.
+    eapply le_lt_trans.
+      apply div_le_mono. apply H. apply (proj1 H').
+      apply (div_lt_upper_bound _ _ _ H). apply H'. }
+  { intros n H0 H1 H2.
+    apply eqb_neq in H0. apply eqb_neq in H1. apply eqb_neq in H2. cbv [id].
+    pose proof (neq_succ_0 1) as H.
+    epose proof log2_spec n _ as H'. rewrite pow_succ_r' in H'.
+    rewrite le_neq. split.
+      apply H'.
+      apply neq_sym. apply H2. }
+  { intros n H0 H1 H2.
+    apply eqb_neq in H0. apply eqb_neq in H1. apply eqb_neq in H2. cbv [id].
+    pose proof (neq_succ_0 1) as H.
+    epose proof log2_spec n _ as H'. rewrite pow_succ_r' in H'.
+    apply sub_lt.
+      apply H'. epose proof mul_lt_mono_pos_l 2 0 (2 ^ log2 n) _.
+      apply H3. eapply lt_trans.
+      apply neq_0_lt. apply neq_sym. apply H0.
+      apply H'. }
+Unshelve. all: omega. Defined.
+
+End Context.
+
+(** The following definitions produce
+    the first few natural numbers as special cases. *)
+
 Section Context.
 
 Context {A : Type} {has_add : HasAdd A} {has_one : HasOne A}.
-
-(** Semirings only come with zero and one,
-    but we can construct any other natural number
-    by adding or multiplying them together.
-    The optimal way to do this is quite complicated,
-    as demonstrated in OEIS sequence A005245.
-    However, we can make the construction more manageable
-    by limiting ourselves to just addition.
-    The following definitions produce an optimal reduction tree (one of many)
-    for constructing the first few natural numbers from additions. *)
 
 Definition two : A := one + one.
 Definition three : A := two + one.
