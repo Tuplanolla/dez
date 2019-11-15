841c497ce68a0eda0ac39104792f40d915218ddd
Sampsa Kiiskinen <tuplanolla@gmail.com>
2019-11-16 00:01:25 +0200

    Add pointless Haskellisms.

--- prototype/Is/PowerSemiring.v
+++ prototype/Is/PowerSemiring.v
@@ -6,6 +6,7 @@ From Maniunfold.Is Require Export
 Class IsPowerSemiring {A : Type} {has_eqv : HasEqv A}
   (has_add : HasAdd A) (has_zero : HasZero A)
   (has_mul : HasMul A) (has_one : HasOne A) (has_pow : HasPow A) : Prop := {
+  pow_is_proper :> IsProper (eqv ==> eqv ==> eqv) pow;
   add_zero_mul_one_is_semiring :> IsSemiring add zero mul one;
   add_mul_is_left_heterodistributive :> IsLeftHeterodistributive add mul pow;
   pow_zero_one_is_right_heteroabsorbing :> IsRightHeteroabsorbing pow zero one;
