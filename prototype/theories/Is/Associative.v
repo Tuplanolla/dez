7566379fd9b7e85c39f76aeb60c61985871bc185
Sampsa Kiiskinen <tuplanolla@gmail.com>
2020-01-20 18:53:05 +0200

    Update diagram harder.

--- prototype/theories/Is/Associative.v
+++ prototype/theories/Is/Associative.v
@@ -1,7 +1,7 @@
 From Maniunfold.Has Require Export
   EquivalenceRelation BinaryOperation.
 From Maniunfold.Is Require Export
-  Equivalence.
+  Equivalence ExternallyAssociative.
 From Maniunfold.ShouldHave Require Import
   EquivalenceRelationNotations AdditiveNotations.
 
@@ -10,3 +10,12 @@ Class IsAssoc {A : Type} {has_eq_rel : HasEqRel A}
   eq_rel_is_eq :> IsEq eq_rel;
   assoc : forall x y z : A, x + (y + z) == (x + y) + z;
 }.
+
+Section Context.
+
+Context {A : Type} `{is_assoc : IsAssoc A}.
+
+Global Instance bin_op_is_ext_assoc : IsExtAssoc bin_op bin_op := {}.
+Proof. apply assoc. Qed.
+
+End Context.
