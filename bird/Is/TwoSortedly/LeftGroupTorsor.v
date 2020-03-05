c24351b6433616cb80ee301c47da550dd87ea6d9
Sampsa Kiiskinen <tuplanolla@gmail.com>
2020-03-05 16:06:51 +0200

    Uniqueness as a property.

--- bird/Is/TwoSortedly/LeftGroupTorsor.v
+++ bird/Is/TwoSortedly/LeftGroupTorsor.v
@@ -2,17 +2,16 @@ From Maniunfold.Has Require Export
   EquivalenceRelation BinaryOperation Unit UnaryOperation
   LeftAction LeftTorsion.
 From Maniunfold.Is Require Export
-  Group LeftGroupAction LeftCompatible.
+  Group LeftGroupAction LeftUnique.
 From Maniunfold.ShouldHave Require Import
   EquivalenceRelationNotations AdditiveNotations.
 
-(** TODO Why group and not something less?
-    Also, what should we call the [uniqueness_property_thing]? *)
+(** TODO Why group and not something less? *)
 
 Class IsLGrpTor {A B : Type}
   {A_has_eq_rel : HasEqRel A} {B_has_eq_rel : HasEqRel B}
   (has_bin_op : HasBinOp A) (has_un : HasUn A) (has_un_op : HasUnOp A)
   (has_l_act : HasLAct A B) (has_l_tor : HasLTor A B) : Prop := {
   bin_op_un_un_op_l_act_is_l_grp_act :> IsLGrpAct bin_op un un_op l_act;
-  uniqueness_property_thing : forall x y : B, (y L- x) L+ x == y;
+  l_act_l_tor_left_uniq :> IsLUniq l_act l_tor;
 }.
---
+++ bird/Is/TwoSortedly/LeftUnique.v
@@ -0,0 +1,8 @@
+From Maniunfold.Has Require Export
+  EquivalenceRelation LeftAction LeftTorsion.
+From Maniunfold.ShouldHave Require Import
+  EquivalenceRelationNotations AdditiveNotations.
+
+Class IsLUniq {A B : Type} {B_has_eq_rel : HasEqRel B}
+  (has_l_act : HasLAct A B) (has_l_tor : HasLTor A B) : Prop :=
+  l_uniq : forall x y : B, (y L- x) L+ x == y.
