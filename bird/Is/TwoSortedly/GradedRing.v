aef549ed9a96fa7e45d47f0ebf56be19282a712d
Sampsa Kiiskinen <tuplanolla@gmail.com>
2020-03-10 15:56:55 +0200

    Sure is dead.

--- bird/Is/TwoSortedly/GradedRing.v
+++ bird/Is/TwoSortedly/GradedRing.v
@@ -21,18 +21,19 @@ Typeclasses Transparent HasGrdOne.
 Notation "x '*' y" := (grd_mul _ _ x y) : algebra_scope.
 Notation "'1'" := (grd_one _) : algebra_scope.
 
+(** TODO Uh oh! *)
+
+Instance eq_has_eq_rel {A : Type} : HasEqRel A := eq.
+
 Class IsGrdRing {A : Type} {P : A -> Type}
-  {A_has_eq_rel : HasEqRel A}
   (A_has_bin_op : HasBinOp A) (A_has_un : HasUn A)
-  (properly : forall x y : A, x == y -> P x -> P y)
   {P_has_eq_rel : forall i : A, HasEqRel (P i)}
   (P_has_add : forall i : A, HasAdd (P i))
   (P_has_zero : forall i : A, HasZero (P i))
   (P_has_neg : forall i : A, HasNeg (P i))
   (has_grd_mul : HasGrdMul A P)
   (has_grd_one : HasGrdOne A P) : Prop := {
-  (* bin_op_is_sgrp :> IsSgrp (A := A) bin_op; *)
-  bin_op_is_mon :> IsMon (A := A) bin_op un;
+  bin_op_un_is_mon :> IsMon (A := A) (has_eq_rel := eq_has_eq_rel) bin_op un;
   add_zero_neg_is_ab_grp :> forall i : A,
     IsAbGrp (P_has_add i) (P_has_zero i) (P_has_neg i);
 
@@ -43,12 +44,12 @@ Class IsGrdRing {A : Type} {P : A -> Type}
   (* x * (y * z) == (x * y) * z; *)
 
   (* grd_l_unl : forall (i : A) (x : P i),
-    (rew [P] l_unl (IsLUnl := bin_op_un_is_l_unl
-      (IsUnl := bin_op_un_is_unl (IsMon := bin_op_is_mon))) i in
-    has_grd_mul 0 i (has_grd_one i) x) == x; *)
+    rew [P] l_unl i in has_grd_mul 0 i (has_grd_one i) x == x; *)
   (* 1 * x == x; *)
 
-  (* grd_r_unl : _; *)
+  (* grd_r_unl : forall (i : A) (x : P i),
+    rew [P] r_unl i in has_grd_mul i 0 x (has_grd_one i) == x; *)
+  (* x * 1 == x; *)
 
   (* ??? *)
   (* add_mul_is_distr :> IsDistr add mul; *)
@@ -62,6 +63,6 @@ Section Context.
 Context {A : Type} {P : A -> Type} `{is_grd_ring : IsGrdRing A P}.
 
 Check forall (i : A) (x : P i),
-  properly (0 + i) i (l_unl i) (has_grd_mul 0 i (has_grd_one i) x) == x.
+  rew [P] l_unl i in has_grd_mul 0 i (has_grd_one i) x == x.
 
 End Context.
