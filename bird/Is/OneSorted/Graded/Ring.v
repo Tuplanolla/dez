a206de1bbaa7ca82ec9b03486c731b82593b4486
Sampsa Kiiskinen <tuplanolla@gmail.com>
2020-03-19 12:47:59 +0200

    Rename things.

--- bird/Is/OneSorted/Graded/Ring.v
+++ bird/Is/OneSorted/Graded/Ring.v
@@ -11,7 +11,8 @@ Class IsGrdRing {A : Type} {P : A -> Type}
   (P_has_zero : forall i : A, HasZero (P i))
   (P_has_neg : forall i : A, HasNeg (P i))
   (P_has_grd_mul : HasGrdMul P) (P_has_grd_one : HasGrdOne P) : Prop := {
-  add_zero_neg_is_ab_grp :> forall i : A, IsAbGrp (A := P i) add zero neg;
+  add_zero_neg_is_ab_grp :> forall i : A,
+    IsAbGrp (P_has_add i) (P_has_zero i) (P_has_neg i);
   add_grd_mul_is_grd_distr :> IsGrdDistr P_has_add grd_mul;
   grd_mul_grd_one_is_grd_mon :> IsGrdMon grd_mul grd_one;
 }.
