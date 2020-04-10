848c4a52c9c2f983856a00a47f610f621000e3e5
Sampsa Kiiskinen <tuplanolla@gmail.com>
2020-04-10 18:58:04 +0300

    More failed tensor things.

--- bird/Provides/TensorTheorems.v
+++ bird/Provides/TensorTheorems.v
@@ -17,163 +17,182 @@ From Maniunfold.ShouldOffer Require Import
 
 Import ListNotations.
 
-Local Open Scope N_scope.
-
 Fixpoint Nseq (start len : N) : list N :=
   map N.of_nat (seq (N.to_nat start) (N.to_nat len)).
+
+(* From Coq Require Import
+  FSets.FMapAVL Structures.OrderedTypeEx.
+Module Import Map := FMapAVL.Make Positive_as_OT. *)
+
+From Coq Require Import
+  FSets.FMapList Structures.OrderedTypeEx.
+Module Import Map := FMapList.Make Positive_as_OT.
+
+From Coq Require Import
+  FSets.FMapFacts.
+
+Module Props := WProperties_fun Positive_as_OT Map.
+Module Mapper := Props.F.
+
+Definition Map_max_key {A : Type} (xs : Map.t A) : option key :=
+  Map.fold (fun (n : key) (x : A) (ms : option key) =>
+    match ms with
+    | Some m => Some (Pos.max n m)
+    | None => Some n
+    end) xs None.
+
+Definition Map_max_key_def {A : Type} (d : key) (xs : Map.t A) : key :=
+  Map.fold (fun (n : key) (x : A) (m : key) => Pos.max n m) xs d.
 
 Section Context.
 
 Context {A B : Type} `{is_two_bimod : IsTwoBimod A B}.
 
-(** We define inhabitants of the tensor algebra in such a way that
-    - [0] becomes [Tensor 0 0 _],
-    - [2] becomes [Tensor 0 2 _],
-    - [(3, 5) x] becomes [Tensor 1 0 (fun n p => (3, 5))],
-    - [2 + (3, 5) x] becomes [Tensor 1 2 (fun n p => (3, 5))].
-    - [2 + (3, 5) x + ((7, 11), (13, 17), (19, 23)) x^3] becomes
-      [Tensor 3 2 (fun n p => match n with
-      | 1 => (3, 5)
-      | 3 => match p with
-        | 0 => (7, 11)
-        | 1 => (13, 17)
-        | 2 => (19, 23)
-        | _ => (0, 0)
-        end
-      | _ => (0, 0)
-      end)]. *)
-
 Record tensor : Type := {
-  deg : N;
-  ground : A;
-  air : N -> N -> B;
-  (* air : forall n : deg, n -> B; *)
+  ht : A;
+  tt : Map.t (list B);
 }.
 
-Definition teqb (p q : tensor) : Prop :=
-  deg p = deg q /\ ground p = ground q /\
-  forall n : N, n < deg p ->
-  forall m : N, m < n ->
-  air p m = air q m.
+Definition proper (p : tensor) : Prop :=
+  forall (k : key) (x : list B),
+  MapsTo k x (tt p) -> length x = Pos.to_nat k.
 
-(** Here is a finite version defined in such a way that
-    - [0] becomes [Tensor 0 []],
-    - [2] becomes [Tensor 2 []],
-    - [(3, 5) x] becomes [Tensor 0 [(3, 5)]],
-    - [2 + (3, 5) x] becomes [Tensor 2 [(3, 5)]].
-    - [2 + (3, 5) x + ((7, 11), (13, 17), (19, 23)) x^3] becomes
-      [Tensor 2 [[],
-        [(3, 5)],
-        [(0, 0), (0, 0)],
-        [(7, 11), (13, 17), (19, 23)]]]. *)
+Definition Add (p q : tensor) : tensor := {|
+  ht := Addition.add (ht p) (ht q);
+  tt := Map.map2 (fun (as' bs : option (list B)) => match as', bs with
+    | Some a, Some b => Some (List.map (uncurry Addition.add) (combine a b))
+    | Some a, None => Some a
+    | None, Some b => Some b
+    | None, None => None
+    end) (tt p) (tt q)
+|}.
 
-Record lensor : Type := {
-  heady : A;
-  taily : list (list B);
-  (* valid : forall n : N, n < length taily -> length (nth n taily) = n; *)
-}.
+Definition Zero : tensor := {|
+  ht := zero;
+  tt := Map.empty (list B);
+|}.
 
-(** TODO Should really use an AVL tree here. *)
+Definition Neg (p : tensor) : tensor :={|
+  ht := neg (ht p);
+  tt := Map.map (List.map neg) (tt p);
+|}.
 
-Definition leqb (p q : lensor) : Prop :=
-  heady p = heady q /\ taily p = taily q.
+(** TODO This is still wrong. *)
 
-Arguments combine {_ _}.
-
-Definition Add (p q : lensor) : lensor :=
-  match p, q with
-  | {| heady := x; taily := xs |}, {| heady := y; taily := ys |} =>
-    {| heady := add x y; taily := map (fun (xys : list B * list B) =>
-        map (fun (xy : B * B) => add (fst xy) (snd xy)) (uncurry combine xys))
-      (combine xs ys) |}
+Definition GrdMul (ps qs : tensor) : tensor :=
+  match ps, qs with
+  | {| ht := pp; tt := p |}, {| ht := qq; tt := q |} => {| ht := pp * qq; tt :=
+    fold_right (fun (k : nat) (r : t (list B)) =>
+      fold_right (fun (l : nat) (s : t (list B)) => match Map.find (Pos.of_nat k) s with
+        | Some u => Map.add (Pos.of_nat k) (app u
+          match Map.find (Pos.of_nat l) p, Map.find (Pos.of_nat (Nat.sub k l)) q with
+          | Some a, Some b => app a b (* List.map (uncurry Addition.add) (combine a b) *)
+          | _, _ => nil
+          end) s
+        | None => Map.add (Pos.of_nat k)
+          match Map.find (Pos.of_nat l) p, Map.find (Pos.of_nat (Nat.sub k l)) q with
+          | Some a, Some b => app a b
+          | _, _ => nil
+          end s
+        end)
+      r (seq (S O) k))
+    (Map.empty (list B))
+    (seq (S O) (Pos.to_nat (Pos.add (Map_max_key_def xH p) (Map_max_key_def xH q)))) |}
   end.
 
-Definition Zero : lensor :=
-  {| heady := 0; taily := [] |}.
+Definition GrdOne : tensor :=
+  {| ht := one; tt := Map.empty (list B) |}.
 
-Definition Neg (p : lensor) : lensor :=
-  match p with
-  | {| heady := x; taily := xs |} =>
-    {| heady := x; taily := map (map neg) xs |}
-  end.
+Definition LAct (a : A) (p : tensor) : tensor :=
+  {| ht := ht p; tt := Map.map (List.map (l_act a)) (tt p) |}.
 
-Definition LAct (a : A) (p : lensor) : lensor :=
-  match p with
-  | {| heady := x; taily := xs |} =>
-    {| heady := x; taily := map (map (l_act a)) xs |}
-  end.
-
-Definition RAct (p : lensor) (a : A) : lensor :=
-  match p with
-  | {| heady := x; taily := xs |} =>
-    {| heady := x; taily := map (map (flip r_act a)) xs |}
-  end.
-
-Arguments app {_}.
-
-(** TODO This product is wrong. *)
-
-Definition GrdBinFn (p q : lensor) : lensor :=
-  match p, q with
-  | {| heady := x; taily := xs |}, {| heady := y; taily := ys |} =>
-    {| heady := mul x y; taily := map (fun (xys : list B * list B) =>
-        uncurry app xys)
-      (combine xs ys) |}
-  end.
-
-Definition GrdMul (p q : A) : A := mul p q.
-
-Definition GrdOne : A := one.
+Definition RAct (p : tensor) (a : A) : tensor :=
+  {| ht := ht p; tt := Map.map (List.map (flip r_act a)) (tt p) |}.
 
 Global Instance N_has_bin_op : HasBinOp N := N.add.
 
 Global Instance N_has_null_op : HasNullOp N := N.zero.
 
-Check IsGrdAlg (A := N) (fun n : N => A) (fun n : N => lensor)
+Check IsGrdAlg (A := N) (fun n : N => A) (fun n : N => tensor)
   (A_has_bin_op := N_has_bin_op) (A_has_null_op := N_has_null_op)
   (fun n : N => Addition.add) (fun n : N => zero) (fun n : N => neg)
-  (fun n p : N => GrdMul) GrdOne
+  (fun n p : N => mul) one
   (fun n : N => Add) (fun n : N => Zero) (fun n : N => Neg)
-  (fun n p : N => GrdBinFn)
+  (fun n p : N => GrdMul)
   (fun n p : N => LAct) (fun n p : N => RAct).
 
 (** Instant tensor algebra; just add water. *)
 
 Global Instance lensor_is_grd_alg :
-  IsGrdAlg (A := N) (fun n : N => A) (fun n : N => lensor)
+  IsGrdAlg (A := N) (fun n : N => A) (fun n : N => tensor)
   (A_has_bin_op := N_has_bin_op) (A_has_null_op := N_has_null_op)
   (fun n : N => Addition.add) (fun n : N => zero) (fun n : N => neg)
-  (fun n p : N => GrdMul) GrdOne
+  (fun n p : N => mul) one
   (fun n : N => Add) (fun n : N => Zero) (fun n : N => Neg)
-  (fun n p : N => GrdBinFn)
+  (fun n p : N => GrdMul)
   (fun n p : N => LAct) (fun n p : N => RAct).
 Proof. repeat split. Abort.
 
 End Context.
 
-Section Tests.
+(* Section Tests.
 
 Local Open Scope Z_scope.
 
-Let p := Build_lensor 2 [[];
-  [(0, 0, 0)];
-  [(1, 0, 0); (1, 0, 0)];
-  [(0, 0, 7); (0, 1, 0); (1, 0, 0)]].
+Instance positive_has_one : HasOne positive := xH.
 
-Let q := Build_lensor 2 [[];
-  [(2, 1, 0)];
-  [(0, 0, 0); (0, 0, 0)];
-  [(0, 1, 0); (0, 1, 0); (0, 0, 1)]].
+Instance Z3_has_add : HasAdd (Z * Z * Z) :=
+  fun x y : Z * Z * Z =>
+  match x, y with
+  | (x0, x1, x2), (y0, y1, y2) => (x0 + y0, x1 + y1, x2 + y2)
+  end.
 
-Let r := Build_lensor 2 [[];
-  [(0, 0, 0)];
-  [(0, 0, 0); (0, 0, 0)];
-  [(1, 0, 0); (1, 0, 0); (2, 1, 0)];
-  [(0, 0, 7); (0, 1, 0); (1, 0, 0); (2, 1, 0)];
-  [(1, 0, 0); (1, 0, 0); (0, 1, 0); (0, 1, 0); (0, 0, 1)];
-  [(0, 0, 7); (0, 1, 0); (1, 0, 0); (0, 1, 0); (0, 1, 0); (0, 0, 1)]].
+Instance Z3_has_neg : HasNeg (Z * Z * Z) :=
+  fun x : Z * Z * Z =>
+  match x with
+  | (x0, x1, x2) => (- x0, - x1, - x2)
+  end.
 
-Compute GrdBinFn p q.
+Instance Z3_has_l_act : HasLAct Z (Z * Z * Z) :=
+  fun (a : Z) (x : Z * Z * Z) =>
+  match x with
+  | (x0, x1, x2) => (a * x0, a * x1, a * x2)
+  end.
+
+Instance Z3_has_r_act : HasRAct Z (Z * Z * Z) :=
+  fun (x : Z * Z * Z) (a : Z) =>
+  match x with
+  | (x0, x1, x2) => (x0 * a, x1 * a, x2 * a)
+  end.
+
+Let p : tensor := {|
+  ht := Z.zero;
+  tt := Props.of_list [
+    (1%positive, [(0, 0, 0)]);
+    (2%positive, [(1, 0, 0); (1, 0, 0)]);
+    (3%positive, [(0, 0, 7); (0, 1, 0); (1, 0, 0)])];
+|}.
+
+Let q : tensor := {|
+  ht := Z.zero;
+  tt := Props.of_list [
+    (1%positive, [(2, 1, 0)]);
+    (2%positive, [(1, 0, 0); (1, 0, 0)]);
+    (3%positive, [(0, 1, 0); (0, 1, 0); (0, 0, 1)])];
+|}.
+
+Let r : tensor := {|
+  ht := Z.zero;
+  tt := Props.of_list [
+    (1%positive, [(0, 0, 0)]);
+    (2%positive, [(0, 0, 0); (0, 0, 0)]);
+    (3%positive, [(1, 0, 0); (1, 0, 0); (2, 1, 0)]);
+    (4%positive, [(0, 0, 7); (0, 1, 0); (1, 0, 0); (2, 1, 0)]);
+    (5%positive, [(1, 0, 0); (1, 0, 0); (0, 1, 0); (0, 1, 0); (0, 0, 1)]);
+    (6%positive, [(0, 0, 7); (0, 1, 0); (1, 0, 0); (0, 1, 0); (0, 1, 0); (0, 0, 1)])];
+|}.
+
+Compute GrdMul p q.
 Compute r.
 
-End Tests.
+End Tests. *)
