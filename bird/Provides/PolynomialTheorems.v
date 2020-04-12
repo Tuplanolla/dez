20e9601f95f32fbf8ee5dd31fde0437c9bc7545d
Sampsa Kiiskinen <tuplanolla@gmail.com>
2020-04-13 00:19:34 +0300

    Now with std++.

--- bird/Provides/PolynomialTheorems.v
+++ bird/Provides/PolynomialTheorems.v
@@ -112,6 +112,16 @@ Global Instance poly_has_l_act : HasLAct A poly := LAct.
 
 Global Instance poly_has_r_act : HasRAct A poly := RAct.
 
+Axiom FMap_extensionality : forall {A : Type} (m n : t A),
+  m = n <-> Equal m n.
+
+Global Instance add_is_assoc : IsAssoc poly Addition.add.
+Proof.
+  intros p q r. cbv [bin_op Addition.add poly_has_add].
+  apply FMap_extensionality. induction p using Props.map_induction_bis.
+  - cbn. apply FMap_extensionality in H. rewrite <- H. apply IHp1.
+  - Abort.
+
 Global Instance add_zero_neg_mul_one_is_ring :
   IsRing poly Add Zero Neg Mul One.
 Proof. repeat split. all: cbv -[Map.t Add Zero Neg Mul One]. Abort.
@@ -179,3 +189,136 @@ Compute eval (Mul p q) 0.
 Compute eval (Mul p q) 3.
 
 End Tests.
+
+(** TODO Try Leibniz maps from std++. *)
+
+From Coq Require Import
+  FunctionalExtensionality.
+From stdpp Require Import
+  option gmap pmap nmap.
+From Maniunfold.Provides Require Import
+  NTheorems.
+From Maniunfold.ShouldHave Require Import
+  OneSorted.ArithmeticNotations.
+From Maniunfold.ShouldOffer Require Import
+  OneSorted.MultiplicativeOperationNotations.
+
+Module QuiteNice.
+
+Section Context.
+
+Context {A : Type} `{is_ring : IsRing A}.
+
+Definition poly : Type := Nmap A.
+
+Definition poly_eval (p : poly) (x : A) : A :=
+  map_fold (fun (i : N) (a b : A) => b + a * x ^ i) 0 p.
+
+Definition poly_add (p q : poly) : poly :=
+  map_zip_with add p q.
+
+Definition poly_zero : poly :=
+  empty.
+
+Definition poly_neg (p : poly) : poly :=
+  fmap neg p.
+
+(** Discrete convolution.
+    We have $c_n = \sum_{i + j = n} a_i \times b_j$. *)
+
+Definition poly_mul (p q : poly) : poly :=
+  map_fold (fun (i : N) (a : A) (r : poly) =>
+    map_fold (fun (j : N) (b : A) (s : poly) =>
+      partial_alter (fun cs : option A =>
+        Some (match cs with
+        | Some c => c
+        | None => 0
+        end + a * b)) (i + j) s) r q) empty p.
+
+Definition poly_one : poly :=
+  singletonM 0 1.
+
+Definition poly_l_act (a : A) (p : poly) : poly :=
+  fmap (mul a) p.
+
+Definition poly_r_act (p : poly) (a : A) : poly :=
+  fmap (flip mul a) p.
+
+Global Instance poly_has_add : HasAdd poly := poly_add.
+
+Global Instance poly_has_zero : HasZero poly := poly_zero.
+
+Global Instance poly_has_neg : HasNeg poly := poly_neg.
+
+Global Instance poly_has_mul : HasMul poly := poly_mul.
+
+Global Instance poly_has_one : HasOne poly := poly_one.
+
+Global Instance poly_has_l_act : HasLAct A poly := poly_l_act.
+
+Global Instance poly_has_r_act : HasRAct A poly := poly_r_act.
+
+Lemma flip_comm : forall (f : A -> A -> A) {is_comm : IsComm A f},
+  flip f = f.
+Proof.
+  intros f ?. extensionality x. extensionality y.
+  cbv [flip]. apply comm. Defined.
+
+Global Instance add_is_comm : IsComm poly add.
+Proof.
+  intros p q. cbv [bin_op Addition.add poly_has_add].
+  cbv [poly_add]. rewrite <- map_zip_with_flip.
+  rewrite flip_comm by typeclasses eauto. reflexivity. Defined.
+
+Global Instance add_is_assoc : IsAssoc poly add.
+Proof.
+  intros p q r. cbv [bin_op Addition.add poly_has_add]. cbv [poly_add]. Abort.
+
+Global Instance add_is_l_unl : IsLUnl poly bin_op one.
+Proof. Abort.
+
+Global Instance add_zero_neg_mul_one_is_ring :
+  IsRing poly add zero neg mul one.
+Proof. repeat split. Abort.
+
+Global Instance add_zero_neg_mul_one_is_unl_assoc_alg :
+  IsUnlAssocAlg A poly add zero neg mul one add zero neg mul one l_act r_act.
+Proof. repeat split. Abort.
+
+Global Instance poly_is_grd_ring : IsGrdRing (fun i : N => A)
+  (fun i : N => Addition.add) (fun i : N => zero) (fun i : N => neg)
+  (fun i j : N => mul) one.
+Proof. repeat split. Abort.
+
+End Context.
+
+Section Tests.
+
+Local Open Scope Z_scope.
+
+(* 42 * x ^ 3 + 7 + 13 * x *)
+Let p : poly := insert (N.add N.one N.two) 42
+  (insert N.zero 7 (insert N.one 13 empty)).
+
+(* 3 * x ^ 4 + x + 5 *)
+Let q : poly := insert (N.add N.two N.two) 3
+  (insert N.one 1 (insert N.zero 5 empty)).
+
+(* 7, 5 *)
+Compute poly_eval p 0.
+Compute poly_eval q 0.
+(* 1180, 251 *)
+Compute poly_eval p 3.
+Compute poly_eval q 3.
+
+(* 12, 1431 *)
+Compute poly_eval (add p q) 0.
+Compute poly_eval (add p q) 3.
+
+(* 35, 296180 *)
+Compute poly_eval (mul p q) 0.
+Compute poly_eval (mul p q) 3.
+
+End Tests.
+
+End QuiteNice.
