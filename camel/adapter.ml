50c52200c2452536bebace01e032e841820a6367
Sampsa Kiiskinen <tuplanolla@gmail.com>
2020-09-03 14:09:11 +0300

    Put the missing adapter back in.

---
+++ camel/adapter.ml
@@ -0,0 +1,40 @@
+open Big_int
+open Extraction
+
+let big_int_of_pos x =
+  let rec f x =
+    match x with
+    | XI y -> add_int_big_int 1 (mult_int_big_int 2 (f y))
+    | XO y -> mult_int_big_int 2 (f y)
+    | XH -> unit_big_int in
+  f x
+
+let big_int_of_z x =
+  match x with
+  | Z0 -> zero_big_int
+  | Zpos y -> big_int_of_pos y
+  | Zneg y -> minus_big_int (big_int_of_pos y)
+
+let pos_of_big_int x =
+  if lt_big_int x unit_big_int then
+    raise (Invalid_argument "nonpositive integer") else
+    let rec f x =
+      let (q, m) = quomod_big_int x (big_int_of_int 2) in
+      if eq_big_int q zero_big_int then
+        XH else
+        if eq_big_int m zero_big_int then
+          XO (f q) else
+          XI (f q) in
+    f x
+
+let z_of_big_int x =
+  let s = sign_big_int x in
+  if s = 0 then
+    Z0 else
+    if s > 0 then
+      Zpos (pos_of_big_int x) else
+      Zneg (pos_of_big_int (minus_big_int x))
+
+let z_of_int x = z_of_big_int (big_int_of_int x)
+
+let string_of_z x = string_of_big_int (big_int_of_z x)
