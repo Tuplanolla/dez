c1928f88d4194319b6601362bf83a69a566f7ca6
Sampsa Kiiskinen <tuplanolla@gmail.com>
2020-12-06 17:03:34 +0200

    All the pairing functions and their shell numberings.

--- fowl/Provides/NaturalPairingFunctions.v
+++ fowl/Provides/NaturalPairingFunctions.v
@@ -12,73 +12,57 @@ Local Opaque "+" "*" "^" "-" "/" sqrt.
 Local Definition seq (n p : N) : list N :=
   map of_nat (seq (to_nat n) (to_nat p)).
 
-(** Triangular number generator. *)
+(** Triangular number function. *)
 
 Definition tri (n : N) : N :=
-  n * (1 + n) / 2.
+  (* n * (1 + n) / 2 *)
+  shiftr (n * succ n) 1.
 
 Arguments tri _ : assert.
 
-(** Inverse of the triangular number generator, with remainder. *)
+(** Inverse of the triangular number function, rounding down. *)
 
-Definition untri_rem (n : N) : N * N :=
-  let ((* square root *) s, (* remainder *) r) := sqrtrem (1 + 8 * n) in
-  let ((* quotient *) q, (* remainder *) r') := div_eucl (s - 1) 2 in
-  (q, r + r'). (* wrong *)
-
-Compute map (untri_rem o tri) (seq 0 32).
-Compute map untri_rem (seq 0 32).
-Compute map ((fun '(p, r) => tri p + r) o untri_rem) (seq 0 32).
-
-Theorem untri_rem_tri (n : N) : snd (untri_rem (tri n)) = 0.
-Proof. Admitted.
-
-Theorem tri_untri_rem (n : N) : (fun '(p, r) => tri p + r) (untri_rem n) = n.
-Proof. Admitted.
-
-(** Inverse of the triangular number generator, rounding down. *)
-
-Definition untri_down (n : N) : N :=
-  (sqrt (1 + 8 * n) - 1) / 2.
-
-Arguments untri_down _ : assert.
-
-Theorem untri_down_tri (n : N) : untri_down (tri n) = n.
-Proof.
-  induction n as [| p eip] using peano_ind.
-  - auto.
-  - cbv [untri_down tri] in *.
-    rewrite add_succ_r. rewrite mul_succ_r. rewrite add_succ_r.
-    rewrite mul_succ_l. rewrite <- divide_div_mul_exact.
-    rewrite (mul_comm 8). rewrite divide_div_mul_exact.
-    replace (8 / 2) with 4 by admit.
-    rewrite mul_succ_l. admit. Admitted.
-
-Theorem tri_untri_down (n : N) : tri (untri_down n) <= n.
-Proof. Admitted.
-
-(** Inverse of the triangular number generator, partial. *)
-
-Definition untri (n : N) : option N :=
-  let (* radicand *) r := 1 + 8 * n in
-  let (* root *) t := sqrt r in
-  if r =? t ^ 2 then Some ((t - 1) / 2) else None.
+Definition untri (n : N) : N :=
+  (* (sqrt (1 + 8 * n) - 1) / 2 *)
+  shiftr (pred (sqrt (succ (shiftl n 3)))) 1.
 
 Arguments untri _ : assert.
 
-Lemma tri_untri_succ (n : N) :
-  option_map tri (untri (succ n)) = option_map (succ o tri) (untri n).
-Proof. Admitted.
-
-Theorem untri_tri (n : N) : untri (tri n) = Some n.
+Theorem untri_tri (n : N) : untri (tri n) = n.
 Proof.
   induction n as [| p eip] using peano_ind.
   - auto.
-  - cbv [untri tri] in *.
-    match goal with
+  - cbv [untri tri] in *. Admitted.
+
+Theorem tri_untri (n : N) : tri (untri n) <= n.
+Proof. Admitted.
+
+(** Inverse of the triangular number function, partial. *)
+
+Definition untri_error (n : N) : option N :=
+  (* let ((* square root *) s, (* remains *) r) := sqrtrem (1 + 8 * n) in
+  if r =? 0 then Some ((s - 1) / 2) else None *)
+  let (s, r) := sqrtrem (succ (shiftl n 3)) in
+  if r =? 0 then Some (shiftr (pred s) 1) else None.
+
+Arguments untri_error _ : assert.
+
+Lemma tri_untri_error_succ (n : N) :
+  option_map tri (untri_error (succ n)) =
+  option_map (succ o tri) (untri_error n).
+Proof. Admitted.
+
+Theorem untri_error_tri (n : N) : untri_error (tri n) = Some n.
+Proof.
+  induction n as [| p eip] using peano_ind.
+  - auto.
+  - cbv [untri_error tri] in *.
+    repeat match goal with
+    | |- context [sqrtrem ?x] => destruct (sqrtrem x) as [ss sr]
+    | _ : context [sqrtrem ?x] |- _ => destruct (sqrtrem x) as [s r]
+    end.
+    repeat match goal with
     | |- context [?x =? ?y] => destruct (eqb_spec x y) as [esp | fsp]
-    end;
-    match goal with
     | _ : context [?x =? ?y] |- _ => destruct (eqb_spec x y) as [ep | fp]
     end.
     + injection eip; clear eip; intros eip.
@@ -88,49 +72,80 @@ Proof.
       exfalso. apply fsp; clear fsp. admit.
     + inversion eip. Admitted.
 
-Theorem tri_untri (n p : N) (e : option_map tri (untri n) = Some p) :
+Theorem tri_untri_error (n p : N)
+  (e : option_map tri (untri_error n) = Some p) :
   n = p.
 Proof.
   generalize dependent p.
   induction n as [| i ei] using peano_ind; intros p enp.
   - cbn in enp. injection enp. auto.
-  - rewrite tri_untri_succ in enp. rewrite option_map_compose in enp.
+  - rewrite tri_untri_error_succ in enp. rewrite option_map_compose in enp.
     rewrite (ei (pred p)).
     try match goal with
     | _ : context [?x =? ?y] |- _ => destruct (eqb_spec x y) as [e | f]
     end.
-    cbv [option_map] in enp. destruct (untri i) eqn : e.
+    cbv [option_map] in enp. destruct (untri_error i) eqn : e.
     + injection enp; clear enp; intros enp. rewrite <- enp at 2. admit.
     + inversion enp. Admitted.
 
 (* Compute map untri (seq 0 32).
 Compute map tri (seq 0 32).
-Compute map (untri o tri) (seq 0 32).
-Compute map (option_map tri o untri) (seq 0 32).
-Compute (filter is_Some o map (option_map tri o untri)) (seq 0 32). *)
+Compute map (untri_error o tri) (seq 0 32).
+Compute map (option_map tri o untri_error) (seq 0 32).
+Compute (filter is_Some o map (option_map tri o untri_error)) (seq 0 32). *)
+
+(** Inverse of the triangular number function, with remainder. *)
+
+Definition untri_rem (n : N) : N * N :=
+  (* let (* quotient *) q := (sqrt (1 + 8 * n) - 1) / 2 in
+  (q, n - tri q) *)
+  let q := shiftr (pred (sqrt (succ (shiftl n 3)))) 1 in
+  (q, n - tri q).
+
+Compute map (prod_uncurry (add o tri) o untri_rem) (seq 0 32).
+Compute map untri_rem (seq 0 32).
+Compute map (untri_rem o tri) (seq 0 32).
+
+Theorem untri_rem_tri (n : N) : snd (untri_rem (tri n)) = 0.
+Proof. Admitted.
+
+Theorem tri_untri_rem (n : N) : prod_uncurry (add o tri) (untri_rem n) = n.
+Proof. Admitted.
 
 Module Cantor.
 
 Module Nonalternating.
 
+Definition pair_shell (n : N) : N :=
+  untri n.
+
+Arguments pair_shell _ : assert.
+
 Definition pair (n : N) : N * N :=
-  let p := untri_down n in
-  let q := n - tri p in
-  (p - q, q).
+  let ((* shell *) s, (* index *) i) := untri_rem n in
+  (s - i, i).
 
 Arguments pair _ : assert.
 
+Definition unpair_shell (n p : N) : N :=
+  n + p.
+
+Arguments unpair_shell _ _ : assert.
+
 Definition unpair (n p : N) : N :=
-  let q := n + p in
-  q * (1 + q) / 2 + p.
+  let (* shell *) s := n + p in
+  s * (1 + s) / 2 + p.
 
 Arguments unpair _ _ : assert.
 
-(* Compute map pair (seq 0 64).
-Compute map (prod_uncurry unpair o pair) (seq 0 64). *)
+Compute map pair (seq 0 64).
+Compute map (prod_uncurry unpair o pair) (seq 0 64).
+
+Compute map pair_shell (seq 0 64).
+Compute map (prod_uncurry unpair_shell o pair) (seq 0 64).
 
 Theorem unpair_pair (n : N) : prod_uncurry unpair (pair n) = n.
-Proof. cbv [prod_uncurry unpair pair]. cbn.  Admitted.
+Proof. cbv [prod_uncurry unpair pair]. cbn. Admitted.
 
 Theorem pair_unpair (n p : N) : pair (prod_uncurry unpair (n, p)) = (n, p).
 Proof. Admitted.
@@ -140,7 +155,7 @@ End Nonalternating.
 Module Alternating.
 
 Definition pair (n : N) : N * N :=
-  let p := untri_down n in
+  let p := untri n in
   let q := n - tri p in
   if even p then
   (p - q, q) else
@@ -151,8 +166,8 @@ Arguments pair _ : assert.
 Definition unpair (n p : N) : N :=
   let q := n + p in
   if even q then
-  div2 (q * (1 + q)) + p else
-  div2 (q * (1 + q)) + n.
+  shiftr (q * (1 + q)) 1 + p else
+  shiftr (q * (1 + q)) 1 + n.
 
 Arguments unpair _ _ : assert.
 
@@ -173,24 +188,42 @@ Module Szudzik.
 
 Module Nonalternating.
 
+Definition pair_shell (n : N) : N :=
+  sqrt n.
+
+Arguments pair_shell _ : assert.
+
 Definition pair (n : N) : N * N :=
-  let q := sqrt n in
-  let r := q * q in
-  if n <? q + r then
-  (q, n - r) else
-  (n - r - q, q).
+  let (* shell *) s := sqrt n in
+  let (* starting point *) t := s ^ 2 in
+  let (* midpoint *) m := t + s in
+  let (* remainder *) r := n - t in
+  if n <? m then
+  (s, r) else
+  (r - s, s).
 
 Arguments pair _ : assert.
 
+Definition unpair_shell (n p : N) : N :=
+  max n p.
+
+Arguments unpair_shell _ _ : assert.
+
 Definition unpair (n p : N) : N :=
+  let (* shell *) s := max n p in
+  let (* starting point *) t := s ^ 2 in
+  let (* midpoint *) m := t + n in
   if p <? n then
-  n * n + p else
-  p * p + n + p.
+  t + p else
+  m + p.
 
 Arguments unpair _ _ : assert.
 
-(* Compute map pair (seq 0 64).
-Compute map (prod_uncurry unpair o pair) (seq 0 64). *)
+Compute map pair (seq 0 64).
+Compute map (prod_uncurry unpair o pair) (seq 0 64).
+
+Compute map pair_shell (seq 0 64).
+Compute map (prod_uncurry unpair_shell o pair) (seq 0 64).
 
 Theorem unpair_pair (n : N) : prod_uncurry unpair (pair n) = n.
 Proof. Admitted.
@@ -244,20 +277,38 @@ Module RosenbergStrong.
 
 Module Nonalternating.
 
+Definition pair_shell (n : N) : N :=
+  sqrt n.
+
+Arguments pair_shell _ : assert.
+
 Definition pair (n : N) : N * N :=
-  let q := sqrt n in
-  if n <? q * q + q then (q, n - q * q) else (q * q + 2 * q - n, q).
+  let (* shell *) s := sqrt n in
+  let (* starting point *) t := s ^ 2 in
+  let (* midpoint *) m := t + s in
+  if n <? m then
+  (s, n - t) else
+  (m + s - n, s).
 
 Arguments pair _ : assert.
 
+Definition unpair_shell (n p : N) : N :=
+  max n p.
+
+Arguments unpair_shell _ _ : assert.
+
 Definition unpair (n p : N) : N :=
-  let q := max n p in
-  q * q + q + p - n.
+  let (* shell *) s := max n p in
+  let (* starting point *) t := s ^ 2 in
+  t + s + p - n.
 
 Arguments unpair _ _ : assert.
 
-(* Compute map pair (seq 0 64).
-Compute map (prod_uncurry unpair o pair) (seq 0 64). *)
+Compute map pair (seq 0 64).
+Compute map (prod_uncurry unpair o pair) (seq 0 64).
+
+Compute map pair_shell (seq 0 64).
+Compute map (prod_uncurry unpair_shell o pair) (seq 0 64).
 
 Theorem unpair_pair (n : N) : prod_uncurry unpair (pair n) = n.
 Proof. Admitted.
@@ -297,3 +348,99 @@ Proof. Admitted.
 End Alternating.
 
 End RosenbergStrong.
+
+Module Hyperbolic.
+
+(** Yes, but does it have a name? *)
+
+Module Nonalternating.
+
+Fixpoint A029837 (n : positive) : N :=
+  match n with
+  | xI p => succ (A029837 p)
+  | xO p => succ (A029837 p)
+  | xH => 0
+  end.
+
+Definition pair_shell (n : N) : N :=
+  (* ceil (log2 (1 + n)) *)
+  A029837 (succ_pos n).
+
+Arguments pair_shell _ : assert.
+
+Fixpoint A000265 (n : positive) : N :=
+  match n with
+  | xI p => Npos n
+  | xO p => A000265 p
+  | xH => Npos n
+  end.
+
+Fixpoint A007814 (n : positive) : N :=
+  match n with
+  | xI p => 0
+  | xO p => succ (A007814 p)
+  | xH => 0
+  end.
+
+Definition pair (n : N) : N * N :=
+  (* (A007814 (1 + n), (A000265 (1 + n) - 1) / 2) *)
+  (A007814 (succ_pos n), shiftr (pred (A000265 (succ_pos n))) 1).
+
+Arguments pair _ : assert.
+
+Definition unpair_shell (n p : N) : N :=
+  (* n + ceil (log2 (1 + 2 * n)) *)
+  n + A029837 (succ_pos (shiftl p 1)).
+
+Arguments unpair_shell _ _ : assert.
+
+Definition unpair (n p : N) : N :=
+  (* 2 ^ n * (2 * p + 1) - 1 *)
+  pred (shiftl 1 n * (succ (shiftl p 1))).
+
+Arguments unpair _ _ : assert.
+
+Compute map pair (seq 0 64).
+Compute map (prod_uncurry unpair o pair) (seq 0 64).
+
+Compute map pair_shell (seq 0 64).
+Compute map (prod_uncurry unpair_shell o pair) (seq 0 64).
+
+Theorem unpair_pair (n : N) : prod_uncurry unpair (pair n) = n.
+Proof. Admitted.
+
+Theorem pair_unpair (n p : N) : pair (prod_uncurry unpair (n, p)) = (n, p).
+Proof. Admitted.
+
+End Nonalternating.
+
+Module Alternating.
+
+Definition pair (n : N) : N * N :=
+  let q := sqrt n in
+  if even q then
+  if n <? q * q + q then (q, n - q * q) else (q * q + 2 * q - n, q) else
+  if n <? q * q + q then (n - q * q, q) else (q, q * q + 2 * q - n).
+
+Arguments pair _ : assert.
+
+Definition unpair (n p : N) : N :=
+  let q := max n p in
+  if even q then
+  q * q + q + p - n else
+  q * q + q + n - p.
+
+Arguments unpair _ _ : assert.
+
+(* Compute map pair (seq 0 64).
+Compute map (prod_uncurry unpair o pair) (seq 0 64). *)
+
+Theorem unpair_pair (n : N) : prod_uncurry unpair (pair n) = n.
+Proof. Admitted.
+
+Theorem pair_unpair (n p : N) : pair (prod_uncurry unpair (n, p)) = (n, p).
+Proof. Admitted.
+
+End Alternating.
+
+End Hyperbolic.
