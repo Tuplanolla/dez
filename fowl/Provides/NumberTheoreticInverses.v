aba3fe6484b5f93919bf9866018eeb003dc419f6
Sampsa Kiiskinen <tuplanolla@gmail.com>
2021-02-09 16:20:11 +0200

    Some corrections.

--- fowl/Provides/NumberTheoreticInverses.v
+++ fowl/Provides/NumberTheoreticInverses.v
@@ -23,6 +23,8 @@ Context (A B : Type) (f : A -> B).
 
 Context `{IsTotOrd A} `{IsTotOrd B} `{IsMon B} `{HasEqDec B}.
 
+Notation "a '<' b" := (a <= b /\ a <> b) (at level 70).
+
 Context (ord_mon : forall (x y z w : B) (l : x <= y) (l' : z <= w), x + z <= y + w).
 
 (** We do not have type classes for successors and predecessors alone,
@@ -55,14 +57,14 @@ Context (unf_remdown_spec :
   (forall (x : A), unf_remdown (f x) = (x, 0)) /\
   (forall (x : B), prod_uncurry (flip _+_ o f) (unf_remdown x) = x) /\
   (forall (x : B), let (y, z) := unf_remdown x in
-    0 <= z /\ z + f y <= f (asuc y) /\ z + f y <> f (asuc y))).
+    0 <= z /\ z + f y < f (asuc y))).
     (* 0 <= z /\ bsuc (z + f y) <= f (asuc y))). *)
 
 Context (unf_down : forall (x : B), A).
 Context (unf_down_spec :
   (forall (x : A), unf_down (f x) = x) /\
   (forall (x : B), f (unf_down x) <= x /\
-    x <= f (asuc (unf_down x)) /\ x <> f (asuc (unf_down x)))).
+    x < f (asuc (unf_down x)))).
   (* (forall (x : B), f (unf_down x) <= x /\ bsuc x <= f (asuc (unf_down x)))). *)
 
 Context (unf_error : forall (x : B), option A).
@@ -77,16 +79,15 @@ Context (rel_down_remdown :
   unf_down x = y).
 Context (rel_error_remdown :
   forall (x : B), let (y, z) := unf_remdown x in
-  (z <> 0 -> unf_error x = Some y) /\
-  (z = 0 -> unf_error x = None)).
+  (z = 0 -> unf_error x = Some y) /\
+  (z <> 0 -> unf_error x = None)).
 
 (** We should be able to prove [unf_down_spec]
     from [unf_remdown_spec] and [rel_down_remdown] and [rel_error_remdown].
     ...and all the five other directions too. *)
 
 Lemma case_0 : (forall (x : A), unf_down (f x) = x) /\
-  (forall (x : B), f (unf_down x) <= x /\
-    x <= f (asuc (unf_down x)) /\ x <> f (asuc (unf_down x))).
+  (forall (x : B), f (unf_down x) <= x /\ x < f (asuc (unf_down x))).
 Proof. clear unf_down_spec unf_error_spec.
   split.
   - intros x.
@@ -130,6 +131,67 @@ Proof. clear unf_down_spec unf_error_spec.
       specialize (e0 y).
       destruct e2 as [e4 [e5 e6]].
       rewrite e1 in e5, e6. rewrite e3. apply e6. Qed.
+
+Lemma case_1 : (forall (x : A), unf_error (f x) = Some x) /\
+  (forall (x y : B) (e : option_map f (unf_error x) = Some y), x = y).
+Proof. clear unf_down_spec unf_error_spec.
+  split.
+  - intros x.
+    destruct unf_remdown_spec as [e0 [e1 e2]].
+    pose proof rel_error_remdown as e3.
+    specialize (e0 x).
+    specialize (e1 (f x)).
+    specialize (e2 (f x)).
+    specialize (e3 (f x)).
+    destruct (unf_remdown (f x)) as [y z] eqn : e.
+    inversion e0; subst.
+    cbv [prod_uncurry compose flip fst snd] in e1.
+    destruct e2 as [e4 e6]. destruct e3 as [e7 e8].
+    apply e7. auto.
+  - intros x a ea.
+    destruct unf_remdown_spec as [e0 [e1 e2]].
+    pose proof rel_error_remdown as e3.
+    specialize (e1 x).
+    specialize (e2 x).
+    specialize (e3 x).
+    cbv [prod_uncurry compose flip fst snd] in e1.
+    destruct (unf_remdown x) as [y z] eqn : e.
+    specialize (e0 y).
+    destruct e2 as [e4 e6].
+    destruct e3 as [e7 e8].
+    destruct (eq_dec z 0).
+    + subst. rewrite l_unl in *.
+      rewrite (e7 (eq_refl 0)) in ea.
+      cbv [option_map] in ea.
+      inversion ea. auto.
+    + rewrite (e8 n) in ea.
+      cbv [option_map] in ea.
+      inversion ea. Qed.
+
+Lemma case_2 : (forall (x : A), unf_remdown (f x) = (x, 0)) /\
+  (forall (x : B), prod_uncurry (flip _+_ o f) (unf_remdown x) = x) /\
+  (forall (x : B), let (y, z) := unf_remdown x in
+    0 < z /\ z + f y < f (asuc y)).
+Proof. clear unf_remdown_spec unf_error_spec.
+  repeat split.
+  - intros x.
+    destruct unf_down_spec as [e0 e1].
+    pose proof rel_down_remdown as e3.
+    specialize (e0 x).
+    specialize (e1 (f x)).
+    destruct e1 as [e4 e5].
+    specialize (e3 (f x)).
+    destruct (unf_remdown (f x)) as [y z] eqn : e.
+    rewrite <- e0, <- e3. f_equal. admit. (* impossible without knowing more *)
+  - intros x.
+    cbv [prod_uncurry compose flip fst snd].
+    destruct unf_down_spec as [e0 e1].
+    pose proof rel_down_remdown as e3.
+    specialize (e1 x).
+    specialize (e3 x).
+    destruct (unf_remdown x) as [y z] eqn : e.
+    specialize (e0 y).
+    destruct e1 as [e7 e8]. admit. Abort.
 
 (** We implement things in terms of each other.
     Only these really require subtraction,
