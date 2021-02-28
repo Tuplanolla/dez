f3a2ea76a0c16faf824ca13fb3d248b0ff5275d3
Sampsa Kiiskinen <tuplanolla@gmail.com>
2021-02-28 14:17:48 +0200

    Good to know this cannot be proven.

--- fowl/Provides/NumberTheoreticInverses.v
+++ fowl/Provides/NumberTheoreticInverses.v
@@ -1,13 +1,13 @@
 From Coq Require Import
-  Lia Lists.List Program.Wf.
+  Lia Lists.List NArith.NArith ZArith.ZArith Program.Wf.
 From Maniunfold.Has Require Export
   OneSorted.EqualityDecision.
 From Maniunfold.Is Require Export
   OneSorted.TotalOrder OneSorted.Monoid.
 From Maniunfold.Provides Require Export
   OptionTheorems ProductTheorems.
-From Maniunfold.ShouldHave Require Import
-  OneSorted.OrderRelationNotations.
+(* From Maniunfold.ShouldHave Require Import
+  OneSorted.OrderRelationNotations. *)
 From Maniunfold.ShouldHave Require Import
   OneSorted.AdditiveNotations.
 
@@ -19,63 +19,137 @@ Import ListNotations.
 
 Section Context.
 
-Context (A B : Type) (f : A -> B).
+Local Open Scope N_scope.
+Local Open Scope Z_scope.
 
-Context `{IsTotOrd A} `{IsTotOrd B} `{IsMon B} `{HasEqDec B}.
-
-Notation "a '<' b" := (a <= b /\ a <> b) (at level 70).
-
-Context (ord_mon : forall (x y z w : B) (l : x <= y) (l' : z <= w), x + z <= y + w).
-
-(** We do not have type classes for successors and predecessors alone,
-    which is why we postulate them here.
-    We might really just want ring structure on [B]. *)
-
-Context (asuc : forall (x : A), A).
-Context (bsuc : forall (x : B), B).
-Context (bpre : forall (x : B), B).
-
-Notation "'1+' a" := (asuc a) (at level 35).
-Notation "'1+' b" := (bsuc b) (at level 35).
-
-(** These properties probably imply more structure anyway. *)
-
-Context (bpre_ord : forall (x : B), bpre x <= x).
-Context (b_wf : well_founded _<=_).
+Let A : Type := N.
+Let B : Type := Z.
 
 (** Our function of interest probably has to be a monotonic injection,
     but this is mere conjecture. *)
 
-Context (f_inj : forall (x y : A) (e : f x = f y), x = y).
-Context (f_mono : forall (x y : A) (l : x <= y), f x <= f y).
-Fail Fail Context (f_surj : forall y : B, exists x : A, y = f x).
+Context (f : A -> B).
+Arguments f _%N.
+Context (f_inj : forall (a b : A) (e : f a = f b), a = b).
+Context (f_mono : forall (a b : A) (l : (a <= b)%N), f a <= f b).
+Fail Fail Context (f_surj : forall b : B, exists a : A, b = f a).
 
 (** We care about three of its pseudoinverses,
     that are specified as follows. *)
 
-Context (unf_error : forall (x : B), option A).
-Context (unf_error_spec :
-  (forall (x : A), unf_error (f x) = Some x) /\
-  (forall (x y : B) (e : option_map f (unf_error x) = Some y), x = y)).
+Context (unf_error : B -> option A).
+Arguments unf_error _%Z.
+Class unf_error_spec : Prop := {
+  here_error : forall a : A, unf_error (f a) = Some a;
+  there_error : forall (x y : B) (e : option_map f (unf_error x) = Some y),
+    x = y;
+}.
+(** This is equally strong. *)
+Class unf_error_spec' : Prop := {
+  here_error' : forall a : A, unf_error (f a) = Some a;
+  there_error' : forall (a : A) (b : B) (e : unf_error b = Some a), f a = b;
+}.
+Lemma from_burritos_strength : unf_error_spec <-> unf_error_spec'.
+Proof.
+  split; intros ?; constructor.
+  - intros a. rewrite here_error. reflexivity.
+  - intros a b e. symmetry. apply there_error.
+    rewrite e. cbv [option_map]. reflexivity.
+  - intros a. rewrite here_error'. reflexivity.
+  - intros x y e. destruct (unf_error x) as [a |] eqn : e'.
+    apply there_error' in e'. cbv [option_map] in e.
+    injection e. clear e. intros e. transitivity (f a); auto.
+    inversion e. Qed.
+(** As demonstrated. *)
+Corollary elsehere_error `{unf_error_spec} :
+  forall x : option A, let y := option_map f x in
+  option_map f (option_bind unf_error y) = y.
+Proof.
+  intros [a |].
+  - cbv zeta. cbv [option_map option_bind]. rewrite here_error. reflexivity.
+  - cbv zeta. cbv [option_map option_bind]. reflexivity. Qed.
+Corollary elsethere_error `{unf_error_spec} :
+  forall x : option B, let y := option_bind unf_error x in
+  option_bind unf_error (option_map f y) = y.
+Proof.
+  intros [b |].
+  - cbv zeta. cbv [option_map option_bind]. destruct (unf_error b) as [a |].
+    + rewrite here_error. reflexivity.
+    + reflexivity.
+  - cbv zeta. cbv [option_map option_bind]. reflexivity. Qed.
+(** We did not need [there_error], which is suspicious.
+    Try a stronger version! *)
+Corollary elsethere_error' `{unf_error_spec} :
+  forall x : option A, option_bind unf_error (option_map f x) = x.
+Proof.
+  intros [a |].
+  - cbv [option_map option_bind]. rewrite here_error. reflexivity.
+  - cbv [option_map option_bind]. reflexivity. Qed.
+(** Curses!
+    Foiled again! *)
 
-Context (unf_down : forall (x : B), A).
-Context (unf_down_spec :
-  (forall (x : A), unf_down (f x) = x) /\
-  (forall (x : B), f (unf_down x) <= x /\ x < f (asuc (unf_down x)))).
+Context (unf_down : B -> A).
+Arguments unf_down _%Z.
+Class unf_down_spec : Prop := {
+  here_down : forall a : A, unf_down (f a) = a;
+  there_down : forall b : B, f (unf_down b) <= b < f (1 + unf_down b);
+}.
 
-Context (unf_remdown : forall (x : B), A + A * B).
-Let f_remdown (x : A + A * B) : B :=
+Definition B_quot : Type := {b : B ! Squash (f (unf_down b) = b)}.
+Program Definition B_pr `{unf_down_spec} (b : B) : B_quot :=
+  Sexists _ (f (unf_down b)) _.
+Next Obligation. intros ? b. apply squash. rewrite here_down. reflexivity. Qed.
+Context (unf_downdep : B_quot -> A).
+Class unf_downdep_spec `{unf_down_spec} : Prop := {
+  here_downdep : forall a : A, unf_downdep (B_pr (f a)) = a;
+  there_downdep : forall x : B_quot, B_pr (f (unf_downdep x)) = x;
+}.
+
+Definition f_remdown (x : A + A * B) : B :=
   match x with
   | inl a => f a
   | inr (a, b) => b + f a
   end.
-Context (unf_remdown_spec :
-  (forall (x : B), match unf_remdown x with
-    | inl y => True
-    | inr (y, z) => f y < z + f y /\ z + f y < f (asuc y)
-    end) /\
-  (forall (x : A), unf_remdown (f x) = inl x) /\
-  (forall (x : B), f_remdown (unf_remdown x) = x)).
+Context (unf_remdown : B -> A + A * B).
+Class unf_remdown_spec : Prop := {
+  refine_remdown : forall x : B,
+    match unf_remdown x with
+    | inl a => True
+    | inr (a, b) => f a < b + f a < f (1 + a)
+    end;
+  here_remdown : forall a : A, unf_remdown (f a) = inl a;
+  there_remdown : forall b : B, f_remdown (unf_remdown b) = b;
+}.
+Remark not_elsehere_remdowndep `{unf_remdown_spec} :
+  ~ forall x : A + A * B, unf_remdown (f_remdown x) = x.
+Proof.
+  intros e.
+  assert (a : A) by constructor.
+  specialize (e (inr (a, 0%Z))).
+  pose proof refine_remdown (f_remdown (inr (a, 0%Z))) as l.
+  rewrite e in l.
+  lia. Qed.
+
+Definition C : Type := A + {x : A * B ! let (a, b) := x in
+  Squash (f a < b + f a < f (1 + a))}.
+Definition f_remdowndep (x : C) : B :=
+  match x with
+  | inl a => f a
+  | inr (Sexists _ (a, b) _) => b + f a
+  end.
+Context (unf_remdowndep : B -> C).
+Class unf_remdowndep_spec : Prop := {
+  here_remdowndep : forall a : A, unf_remdowndep (f a) = inl a;
+  there_remdowndep : forall b : B, f_remdowndep (unf_remdowndep b) = b;
+}.
+Corollary elsehere_remdowndep `{unf_remdowndep_spec} :
+  forall x : C, unf_remdowndep (f_remdowndep x) = x.
+Proof.
+  intros [a | [[a b] e]].
+  - cbv [f_remdowndep]. rewrite here_remdowndep. reflexivity.
+  - cbv [f_remdowndep].
+    destruct (unf_remdowndep (b + f a)) as [a' | [[a' b'] e']] eqn : e''.
+    exfalso. admit. f_equal. apply Spr1_inj. cbv [Spr1]. admit. Abort.
 
 (** We implement things in terms of each other.
     Only these really require subtraction,
--- fowl/Provides/OptionTheorems.v
+++ fowl/Provides/OptionTheorems.v
@@ -4,6 +4,13 @@ From Maniunfold Require Export
 Definition is_Some (A : Type) (x : option A) : bool :=
   if x then true else false.
 
+Definition option_bind (A B : Type)
+  (f : A -> option B) (x : option A) : option B :=
+  match x with
+  | Some a => f a
+  | None => None
+  end.
+
 Lemma option_map_id (A : Type) (x : option A) :
   option_map id x = x.
 Proof. destruct x as [a |]; auto. Qed.
