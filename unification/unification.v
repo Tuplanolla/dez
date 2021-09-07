82e4019582814a89d16d2f0f264ee86adc77b187
Sampsa Kiiskinen <tuplanolla@gmail.com>
2021-09-07 15:21:11 +0300

    Tree generation.

--- unification/unification.v
+++ unification/unification.v
@@ -18,6 +18,27 @@ Inductive term : Set :=
 
 Scheme Equality for term.
 
+(* Scheme Partial Order for term. *)
+
+(** This is somewhat nonsensical. *)
+
+Fixpoint term_leb (x y : term) : bool :=
+  match x, y with
+  | var n, var p => Nat.leb n p
+  | var _, _ => true
+  | un _ _, var _ => false
+  | un n x, un p y => Nat.leb n p && term_leb x y
+  | un _ _, _ => true
+  | bin _ _ _, var _ => false
+  | bin _ _ _, un _ _ => false
+  | bin n x y, bin p z w => Nat.leb n p && term_leb x z && term_leb y w
+  | bin _ _ _, _ => true
+  | rel _ _, var _ => false
+  | rel _ _, un _ _ => false
+  | rel _ _, bin _ _ _ => false
+  | rel x y, rel z w => term_leb x z && term_leb y w
+  end.
+
 Fixpoint wf_fix (b : bool) (x : term) : bool :=
   match x with
   | var _ => b
@@ -63,6 +84,9 @@ Compute bsub
 Compute bsub
   (rel (var 1) (var 0))
   (rel (bin 0 (var 0) (var 1)) (var 0)).
+
+Class HasDef (A : Type) : Type :=
+  def : A.
 
 Class HasMap (F : Type -> Type) : Type :=
   map (A B : Type) (f : A -> B) (x : F A) : F B.
@@ -114,11 +138,11 @@ Module IndexedTermNotations.
 Notation "'x_' n" := (var n)
   (at level 0, n at level 100, format "'x_' n", only printing).
 Notation "'-_' n x" := (un n x)
-  (at level 50, format "-_ n  x", only printing).
+  (at level 50, format "'-_' n  x", only printing).
 Notation "x '+_' n y" := (bin n x y)
-  (at level 50, format "x  +_ n  y", only printing).
+  (at level 50, format "x  '+_' n  y", only printing).
 Notation "x '=' y" := (rel x y)
-  (at level 70, format "x  =  y", only printing).
+  (at level 70, format "x  '='  y", only printing).
 
 End IndexedTermNotations.
 
@@ -126,12 +150,12 @@ Module TermNotations.
 
 Notation "'x'" := (var _)
   (at level 0, format "'x'", only printing).
-Notation "'-'' x" := (un _ x)
-  (at level 50, format "-'  x", only printing).
-Notation "x '+'' y" := (bin _ x y)
-  (at level 50, format "x  +'  y", only printing).
+Notation "'-_{}' x" := (un _ x)
+  (at level 50, format "'-_{}'  x", only printing).
+Notation "x '+_{}' y" := (bin _ x y)
+  (at level 50, format "x  '+_{}'  y", only printing).
 Notation "x '=' y" := (rel x y)
-  (at level 70, format "x  =  y", only printing).
+  (at level 70, format "x  '='  y", only printing).
 
 End TermNotations.
 
@@ -171,6 +195,143 @@ Import TermNotations.
 Compute gen 0.
 Compute gen 1.
 Compute gen 2.
+
+End Context.
+
+(** Rose tree. *)
+
+Inductive tree (A : Type) : Type := Node {
+  leaf : A;
+  branches : list (tree A);
+}.
+
+Notation "a '<:' x" := (Node a x) (at level 50).
+
+Scheme Equality for list.
+Fail Scheme Equality for tree.
+
+Fixpoint tree_beq (A : Type) (beq : A -> A -> bool) (x y : tree A) : bool :=
+  match x, y with
+  | Node a x, Node b y => beq a b && list_beq (tree_beq beq) x y
+  end.
+
+Fixpoint map_tree (A B : Type) (f : A -> B) (x : tree A) : tree B :=
+  match x with
+  | Node x ts => Node (f x) (map (map_tree f) ts)
+  end.
+
+#[local] Instance tree_has_map : HasMap tree := @map_tree.
+
+Definition tree_pure (A : Type) (x : A) : tree A :=
+  Node x [].
+
+#[local] Instance tree_has_pure : HasPure tree := @tree_pure.
+
+Fixpoint tree_bind (A B : Type) (f : A -> tree B) (x : tree A) : tree B :=
+  match x with
+  | Node x ts =>
+    match f x with
+    | Node x' ts' => Node x' (ts' ++ map (tree_bind f) ts)
+    end
+  end.
+
+#[local] Instance tree_has_bind : HasBind tree := @tree_bind.
+
+Definition tree_join (A : Type) (x : tree (tree A)) : tree A :=
+  x >>= id.
+
+#[local] Instance tree_has_join : HasJoin tree := @tree_join.
+
+From Equations.Prop Require Import Equations.
+
+Equations unfold_tree (A B : Type) (n : nat) (x : A)
+  (f : B -> A * list B) (b : B) : tree A by struct n :=
+  unfold_tree O x f b := Node x [];
+  unfold_tree (S n) x f b :=
+    let (a, bs) := f b in
+    Node a (map (unfold_tree n x f) bs).
+
+Compute unfold_tree 8 _
+  (fun x => if Nat.ltb 7 (2 * x + 1) then (x, []) else (x, [2 * x; 2 * x + 1]))
+  1.
+
+Compute unfold_tree 8 _
+  (fun '(m, x) => match m with
+  | O => (x, [])
+  | S m' => (x, [(m', un 0 x); (m', bin 0 x x)])
+  end)
+  (2, var 0).
+
+(** Generate all unbalanced subtrees of depth below [m] in hierarchical order,
+    using only one variable. *)
+
+Fixpoint gen_tree_fix (m : nat) : list (tree term) :=
+  match m with
+  | O => []
+  | S m' =>
+    let vars := [] in
+    let uns :=
+      gen_tree_fix m' >>= fun '(Node a ys) =>
+      [Node (un 0 a) ys] in
+    let bins :=
+      gen_tree_fix m' >>= fun '(Node a ys) =>
+      gen_tree_fix m' >>= fun '(Node b zs) =>
+      [Node (bin 0 a b) (ys ++ zs)] in
+    [Node (var 0) vars;
+    Node (un 0 (var 0)) uns;
+    Node (bin 0 (var 0) (var 0)) bins]
+  end.
+
+Definition gen_tree (m : nat) : tree term :=
+  Node (var 0) (gen_tree_fix m).
+
+Compute gen_tree 0. Compute var 0 <: [].
+Compute gen_tree 1. Compute var 0 <: [
+  var 0 <: [];
+  un 0 (var 0) <: [];
+  bin 0 (var 0) (var 0) <: []].
+Compute gen_tree 2. Compute var 0 <: [
+  var 0 <: [];
+  un 0 (var 0) <: [
+    un 0 (var 0) <: [];
+    un 0 (un 0 (var 0)) <: [];
+    un 0 (bin 0 (var 0) (var 0)) <: []];
+  bin 0 (var 0) (var 0) <: [
+    bin 0 (var 0) (var 0) <: [];
+    bin 0 (var 0) (un 0 (var 0)) <: [];
+    bin 0 (var 0) (bin 0 (var 0) (var 0)) <: [];
+    bin 0 (un 0 (var 0)) (var 0) <: [];
+    bin 0 (un 0 (var 0)) (un 0 (var 0)) <: [];
+    bin 0 (un 0 (var 0)) (bin 0 (var 0) (var 0)) <: [];
+    bin 0 (bin 0 (var 0) (var 0)) (var 0) <: [];
+    bin 0 (bin 0 (var 0) (var 0)) (un 0 (var 0)) <: [];
+    bin 0 (bin 0 (var 0) (var 0)) (bin 0 (var 0) (var 0)) <: []]].
+
+Section Context.
+
+Import TermNotations.
+
+Compute gen_tree 0. Compute var 0 <: [].
+Compute gen_tree 1. Compute var 0 <: [
+  var 0 <: [];
+  un 0 (var 0) <: [];
+  bin 0 (var 0) (var 0) <: []].
+Compute gen_tree 2. Compute var 0 <: [
+  var 0 <: [];
+  un 0 (var 0) <: [
+    un 0 (var 0) <: [];
+    un 0 (un 0 (var 0)) <: [];
+    un 0 (bin 0 (var 0) (var 0)) <: []];
+  bin 0 (var 0) (var 0) <: [
+    bin 0 (var 0) (var 0) <: [];
+    bin 0 (var 0) (un 0 (var 0)) <: [];
+    bin 0 (var 0) (bin 0 (var 0) (var 0)) <: [];
+    bin 0 (un 0 (var 0)) (var 0) <: [];
+    bin 0 (un 0 (var 0)) (un 0 (var 0)) <: [];
+    bin 0 (un 0 (var 0)) (bin 0 (var 0) (var 0)) <: [];
+    bin 0 (bin 0 (var 0) (var 0)) (var 0) <: [];
+    bin 0 (bin 0 (var 0) (var 0)) (un 0 (var 0)) <: [];
+    bin 0 (bin 0 (var 0) (var 0)) (bin 0 (var 0) (var 0)) <: []]].
 
 End Context.
 
@@ -246,6 +407,8 @@ Definition gensym_bin (tt : unit) : Fresh nat :=
 
 End Context.
 
+#[local] Instance names_has_def : HasDef Names := no_names.
+
 #[local] Instance fresh_has_map (St : Type) : HasMap (State St) :=
   @fresh_map St.
 
@@ -279,13 +442,16 @@ Fixpoint label (t : term) : Fresh term :=
     pure (rel x y)
   end.
 
-Definition relabel (t : term) : term := run (label t) no_names.
+Definition relabel (t : term) : term := run (label t) def.
 
 End Context.
 
-Compute map relabel (gen 0).
-Compute map relabel (gen 1).
-Compute map relabel (gen 2).
+Compute map relabel (gen_all 0).
+Compute map relabel (gen_all 1).
+Compute map relabel (gen_all 2).
+
+Compute map (uncurry term_beq)
+  (combine (map relabel (gen_all 1)) (map relabel (gen_all 2))).
 
 Section Context.
 
@@ -294,5 +460,31 @@ Import IndexedTermNotations.
 Compute map relabel (gen_all 0).
 Compute map relabel (gen_all 1).
 Compute map relabel (gen_all 2).
+
+End Context.
+
+Module CursedTermNotations.
+
+Notation "'var_' n" := (var n)
+  (at level 0, n at level 100, format "'var_' n").
+Notation "'un_' n x" := (un n x)
+  (at level 100, x at level 0, format "'un_' n  x").
+Notation "'bin_' n x y" := (bin n x y)
+  (at level 100, x at level 0, y at level 0, format "'bin_' n  x  y").
+
+End CursedTermNotations.
+
+Section Context.
+
+Import CursedTermNotations.
+
+Compute map relabel (gen_all 1).
+
+Check let T0 := ?[T0] in fun
+  (var_0 var_1 var_2 : _)
+  (un_0 : _ -> _)
+  (bin_0 : _ -> _ -> _)
+  (rel : _ -> _ -> Prop) =>
+  rel (bin_0 var_0 var_1) (un_0 var_2).
 
 End Context.
