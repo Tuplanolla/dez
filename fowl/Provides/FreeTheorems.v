c708bbb4bb3a03afeb729105e95cd382a9eda177
Sampsa Kiiskinen <tuplanolla@gmail.com>
2021-11-04 21:26:40 +0200

    Perhaps better free stuff.

--- fowl/Provides/FreeTheorems.v
+++ fowl/Provides/FreeTheorems.v
@@ -1,13 +1,13 @@
 (** * Free Structure *)
 
 From Coq Require Import
-  Extraction Lists.List ZArith.ZArith.
+  Extraction Lia Lists.List ZArith.ZArith.
 From DEZ.Has Require Export
   Decidability Unsquashing.
 From DEZ.Is Require Export
   Group Truncated.
 From DEZ.Provides Require Export
-  BooleanTheorems UnitTheorems ZTheorems.
+  BooleanTheorems ProductTheorems UnitTheorems ZTheorems.
 From DEZ.ShouldHave Require Import
   EquivalenceRelationNotations AdditiveNotations.
 
@@ -57,70 +57,124 @@ Proof.
 
 Import ListNotations.
 
-Section Context.
+Arguments rev {_} _.
 
-Context (A : Type) (d : forall x y : A, {x = y} + {x <> y}).
+Lemma rev_nil (A : Type) (a : list A)
+  (s : rev a = []) : a = [].
+Proof.
+  apply (f_equal rev) in s. rewrite rev_involutive in s.
+  unfold rev in s. apply s. Qed.
 
-#[local] Instance has_eq_dec : HasEqDec A := d.
+Lemma rev_cons (A : Type) (x : A) (a b : list A)
+  (s : rev a = x :: b) : a = rev b ++ [x].
+Proof.
+  apply (f_equal rev) in s. rewrite rev_involutive in s.
+  change (rev (x :: b)) with (rev b ++ [x]) in s. apply s. Qed.
 
-Equations wfb (t : (bool * A) * (bool * A)) : bool :=
-  wfb ((x, a), (y, b)) := decide (~ (x <> y /\ a = b)).
+Lemma combine_snoc (A B : Type)
+  (x : A) (a : list A) (y : B) (b : list B) (s : length a = length b) :
+  combine (a ++ [x]) (b ++ [y]) = combine a b ++ [(x, y)].
+Proof.
+  revert y b s.
+  induction a as [| z c t]; intros y [| w d] s.
+  - reflexivity.
+  - inversion s.
+  - inversion s.
+  - inversion s as [s']. pose proof fun y : B => t y _ s' as t'.
+    cbn [combine app]. f_equal. apply t'. Qed.
 
-Equations free_wfb (s : list (bool * A)) : bool :=
-  free_wfb s := decide (Forall wfb (combine s (skipn 1 s))).
+Lemma combine_rev (A B : Type)
+  (a : list A) (b : list B) (s : length a = length b) :
+  combine (rev a) (rev b) = rev (combine a b).
+Proof.
+  revert b s.
+  induction a as [| x c t]; intros [| y d] s.
+  - reflexivity.
+  - inversion s.
+  - inversion s.
+  - inversion s as [s']. pose proof t _ s' as t'.
+    cbn [combine rev]. rewrite combine_snoc.
+    + f_equal. apply t'.
+    + repeat rewrite rev_length. apply s'. Qed.
 
 (** We should use finger trees for this.
     It is possible to achieve constant time cons, snoc,
     logarithmic time append and linear time iteration.
     See the paper by Hinze and Paterson for details. *)
 
+Section Context.
+
+Context (A : Type) {d : HasEqDec A}.
+
+Equations wfb (a : (bool * A) * (bool * A)) : bool :=
+  wfb ((i, x), (j, y)) := decide (~ (i <> j /\ x = y)).
+
+Equations free_wfb (s : list (bool * A)) : bool :=
+  free_wfb s := decide (Forall wfb (combine s (skipn 1 s))).
+
 Equations free : Type :=
   free := {s : list (bool * A) $ Squash (free_wfb s)}.
 
-Equations rel (s t : free) : Prop :=
-  rel (Sexists _ s _) (Sexists _ t _) := is_left (eq_dec s t).
+#[local] Notation "'(' x ';' a ')'" := (Sexists _ x a).
+
+Equations rel (x y : free) : Prop :=
+  rel (s; _) (t; _) := decide (s = t).
 
 Equations null : free :=
-  null := Sexists _ [] _.
+  null := ([]; _).
 Next Obligation. apply squash. reflexivity. Qed.
 
-Equations un_fix (t s : list (bool * A)) : list (bool * A) by struct s :=
-  un_fix t [] := t;
-  un_fix t ((x, a) :: s) := un_fix ((negb x, a) :: t) s.
-
-Equations un (s : free) : free :=
-  un (Sexists _ s _) := Sexists _ (un_fix [] s) _.
+Equations un (x : free) : free :=
+  un (s; _) := (map (prod_bimap negb id) (rev s); _).
 Next Obligation.
-  intros s w. apply unsquash in w. unfold free_wfb in w.
-  apply Decidable_sound in w. apply squash. apply Decidable_complete.
-  change (fun t : (bool * A) * (bool * A) => is_true (wfb t))
+  intros s ws.
+  apply unsquash in ws. apply Decidable_sound in ws.
+  apply squash. apply Decidable_complete.
+  change (fun x : (bool * A) * (bool * A) => is_true (wfb x))
   with (is_true o wfb) in *.
-  induction s as [| [x a] [| [y b] t] v].
-  - unfold combine, un_fix, skipn. apply Forall_nil.
-  - unfold combine, un_fix, skipn. apply Forall_nil.
-  - replace (combine ((x, a) :: (y, b) :: t) (skipn 1 ((x, a) :: (y, b) :: t)))
-    with (((x, a), (y, b)) :: combine ((y, b) :: t) t) in w by reflexivity.
-    pose proof Forall_inv w as u.
-    pose proof v (Forall_inv_tail w) as u_tail. Admitted.
+  induction s as [| [i x] [| [j y] u] w].
+  - apply Forall_nil.
+  - apply Forall_nil.
+  - rewrite map_rev. rewrite combine_firstn_r.
+  remember (map (prod_bimap negb id) ((i, x) :: (j, y) :: u)) as a.
+    rewrite skipn_rev.
+    rewrite rev_length.
+    rewrite firstn_rev.
+    rewrite firstn_length_le by lia.
+    replace (length a - (length a - 1)) with 1%nat.
+    2:{ subst a. cbn. destruct (length (map (prod_bimap negb id) u)); lia. }
+    Search firstn length.
+    subst a.
+    rewrite map_length.
+    rewrite combine_rev.
+    2:{ cbn. rewrite map_length. f_equal. rewrite firstn_length_le. reflexivity.
+      cbn. rewrite map_length. lia. }
+    apply Forall_rev. cbn.
+    apply Forall_cons.
+    + apply Forall_inv in ws. apply Decidable_sound in ws.
+      apply Decidable_complete. intros [ji yx]. apply ws.
+      split. intros ij. apply ji. apply f_equal. auto. auto.
+    + apply Forall_inv_tail in ws.
+      apply w in ws. clear w. Admitted.
 
 Equations bin_fix (s t : list (bool * A)) :
   list (bool * A) * list (bool * A) by struct t :=
   bin_fix [] b := ([], b);
   bin_fix a [] := (a, []);
-  bin_fix ((i, x) :: xs) ((j, y) :: ys) with decide (i = j /\ x = y) := {
-    | true => bin_fix xs ys;
-    | false => ((i, x) :: xs, (j, y) :: ys)
+  bin_fix ((i, x) :: a) ((j, y) :: b) with decide (i <> j /\ x = y) := {
+    | true => bin_fix a b
+    | false => ((i, x) :: a, (j, y) :: b)
   }.
 
-Equations bin (s t : free) : free :=
-  bin (Sexists _ a _) (Sexists _ b _) :=
-  let (s, t) := bin_fix (un_fix [] a) b in
-  Sexists _ (un_fix [] s ++ t) _.
+Equations bin (x y : free) : free :=
+  bin (s; _) (t; _) :=
+  let (s', t') := bin_fix (rev s) t in
+  (rev s' ++ t'; _).
 Next Obligation.
-  intros a wa b wb s t.
-  apply unsquash in wa, wb. unfold free_wfb in wa, wb.
-  apply Decidable_sound in wa, wb. apply squash. apply Decidable_complete.
-  change (fun t : (bool * A) * (bool * A) => is_true (wfb t))
+  intros s ws t wt s' t'.
+  apply unsquash in ws, wt. apply Decidable_sound in ws, wt.
+  apply squash. apply Decidable_complete.
+  change (fun x : (bool * A) * (bool * A) => is_true (wfb x))
   with (is_true o wfb) in *. Admitted.
 
 #[local] Instance is_grp : IsGrp rel null un bin.
@@ -144,23 +198,21 @@ Proof.
 
 End Context.
 
+Arguments free _ {_}.
+
 Section Context.
 
-Context (A : Type) (d : forall x y : A, {x = y} + {x <> y}) (f : A -> Z).
-
-#[local] Instance again_has_eq_dec : HasEqDec A := d.
-#[local] Instance Z_has_eq_dec : HasEqDec Z := Z.eq_dec.
+Context (A : Type) {d : HasEqDec A} (f : A -> Z).
 
 #[local] Open Scope Z_scope.
 
-Definition hm (s : free d) : Z :=
+Definition hm (s : free A) : Z :=
   fold_right Z.add Z.zero
   (map (fun t : bool * A => let (x, a) := t in
   if x then Z.opp (f a) else f a) (Spr1 s)).
 
 #[local] Instance is_grp_hom :
-  IsGrpHom (rel eq_dec) (null eq_dec) (un eq_dec) (bin eq_dec)
-  eq Z.zero Z.opp Z.add hm.
+  IsGrpHom rel null un bin eq Z.zero Z.opp Z.add hm.
 Proof.
   esplit.
   - apply is_grp.
@@ -192,6 +244,8 @@ End Context.
 
 End Mess.
 
+Import ListNotations.
+
 #[local] Open Scope Z_scope.
 
 (* - (b a' c a c' b b) *)
@@ -204,9 +258,7 @@ Compute
   let a' := (negb (fst a), snd a) in
   let b' := (negb (fst b), snd b) in
   let c' := (negb (fst c), snd c) in
-  un (Sexists_has_eq_dec Z.eq_dec)
-  (Sexists (Squash o free_wfb Z.eq_dec)
-  [b; a'; c; a; c'; b; b] _).
+  Mess.un (Sexists _ [b; a'; c; a; c'; b; b] _).
 
 (* b a' c a' + a c' b b *)
 (* b a' c + c' b b *)
@@ -220,7 +272,7 @@ Compute
   let a' := (negb (fst a), snd a) in
   let b' := (negb (fst b), snd b) in
   let c' := (negb (fst c), snd c) in
-  bin Z.eq_dec [b; a'; c; a'] [a; c'; b; b].
+  Mess.bin [b; a'; c; a'] [a; c'; b; b].
 
 Compute
   let a := (false, 1) in
@@ -230,6 +282,6 @@ Compute
   let b' := (negb (fst b), snd b) in
   let c' := (negb (fst c), snd c) in
   (fold_right Z.add Z.zero [2; -1; 3; -1; 1; -3; 2; 2],
-  hm (bin Z.eq_dec [b; a'; c; a'] [a; c'; b; b])).
+  Mess.hm (Mess.bin [b; a'; c; a'] [a; c'; b; b])).
 
 (* Extraction Mess. *)
