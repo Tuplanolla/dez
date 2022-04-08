10ee6acb270104727abb11ec2cabb5b8ddc51ec7
Sampsa Kiiskinen <tuplanolla@gmail.com>
2022-04-08 14:56:36 +0300

    It is unclear how to use the equations plugin.

--- fowl/Is/Finite.v
+++ fowl/Is/Finite.v
@@ -13,10 +13,15 @@ From DEZ.Provides Require Import
 From DEZ.Supports Require Import
   EquivalenceNotations.
 
+Equations is_left (A B : Prop) (a : {A} + {B}) : bool :=
+  is_left (left _) := true;
+  is_left (right _) := false.
+
 Import ListNotations.
 
 #[local] Open Scope core_scope.
 #[local] Open Scope N_scope.
+#[local] Open Scope sig_scope.
 
 (** ** Unsorted Enumeration of a Type *)
 
@@ -75,7 +80,7 @@ Section Context.
 
 Context (A : Type).
 
-(** This is simpler to prove things about. *)
+(** This is simpler to use in proofs. *)
 
 Equations index (a : list A) : list (N * A) :=
   index a := combine (map N.of_nat (seq 0 (length a))) a.
@@ -89,7 +94,7 @@ Proof.
   rewrite combine_length. rewrite map_length. rewrite seq_length.
   rewrite Min.min_idempotent. reflexivity. Qed.
 
-Lemma index_ed (a : list A) (p : N) (x : A) (s : In (p, x) (index a)) :
+Lemma index_In (a : list A) (p : N) (x : A) (s : In (p, x) (index a)) :
   p < N.of_nat (length a).
 Proof.
   unfold index in s. apply in_combine_l in s. apply (in_map N.to_nat) in s.
@@ -99,13 +104,13 @@ Proof.
 
 End Context.
 
-Module Better.
+Module Faster.
 
 Section Context.
 
 Context (A : Type).
 
-(** This is computationally better-behaved. *)
+(** This is faster to use in computations. *)
 
 Fail Fail Equations index_fix (n : N) (a : list A) : list (N * A) :=
   index_fix _ [] := [];
@@ -114,19 +119,15 @@ Fail Fail Equations index_fix (n : N) (a : list A) : list (N * A) :=
 Equations index (a : list A) : list (N * A) :=
   index := index_fix 0 where
   index_fix (n : N) (a : list A) : list (N * A) :=
-    index_fix _ [] := [];
-    index_fix n (x :: b) := (n, x) :: index_fix (N.succ n) b.
+  index_fix _ [] := [];
+  index_fix n (x :: b) := (n, x) :: index_fix (N.succ n) b.
 
 #[export] Instance index_fix_has_enum
   {a : HasEnum A} : HasEnum (N * A) := index (enum A).
 
 End Context.
 
-End Better.
-
-Lemma proj1_sig_inj (A : Type) (P : A -> Prop)
-  (a b : {x : A | P x}) (s : proj1_sig a = proj1_sig b) : a = b.
-Proof. Admitted.
+End Faster.
 
 Section Context.
 
@@ -138,31 +139,33 @@ Ltac note := progress (
   denote d with (equiv_dec (A := A));
   denote a with (enum A)).
 
-#[local] Open Scope sig_scope.
-
 Equations encode (s : {p : N | p < N.of_nat (length (enum A))}) : A :=
   encode (p; t) with (enum A; eq_refl) := {
-    | ([]; _) := _
-    | (x :: b; _) := snd (nth (N.to_nat p) (index b) (0, x))
+    | ([]; _) => _
+    | (x :: b; _) => snd (nth (N.to_nat p) (index b) (0, x))
   }.
 Next Obligation.
   intros p t u. cbv beta in t. rewrite u in t.
   unfold length, N.of_nat in t. lia. Qed.
 
-Definition is_left (A B : Prop) (x : {A} + {B}) : bool :=
-  if x then true else false.
+Equations encode_without_inspect (s : {p : N | p < N.of_nat (length a)}) : A :=
+  encode_without_inspect := encode_without_inspect_fix (enum A) where
+  encode_without_inspect_fix (a : list A) (s : {p : N | p < N.of_nat (length a)}) : A :=
+  encode_without_inspect_fix [] _ := _;
+  encode_without_inspect_fix (x :: b) (p; _) := snd (nth (N.to_nat p) (index b) (0, x)).
+Next Obligation. unfold length, N.of_nat; intros []; lia. Qed.
 
 Equations matching (x : A) (s : N * A) : bool :=
   matching x (_, y) := is_left (d x y).
 
 Equations decode (x : A) : {p : N | p < N.of_nat (length (enum A))} :=
   decode x with (find (matching x) (index (enum A)); eq_refl) := {
-    | (Some (p, y); _) := (p; _)
-    | (None; _) := _
+    | (Some (p, y); _) => (p; _)
+    | (None; _) => _
   }.
 Next Obligation with note.
   intros x p y t...
-  apply find_some in t. destruct t as [u v]. apply index_ed in u. lia. Qed.
+  apply find_some in t. destruct t as [u v]. apply index_In in u. lia. Qed.
 Next Obligation with note.
   intros x t... exfalso.
   pose proof full x as u.
@@ -182,18 +185,59 @@ Next Obligation with note.
   apply nth_In.
   rewrite index_length... lia. Qed.
 
+Equations decode_fst_without_inspect (x : A) : N :=
+  decode_fst_without_inspect x with find (matching x) (index (enum A)) := {
+    | Some (p, y) => p
+    | None => 0
+  }.
+
+Lemma decode_snd_without_inspect (x : A) :
+  decode_fst_without_inspect x < N.of_nat (length (enum A)).
+Proof with note.
+  apply_funelim (decode_fst_without_inspect x). all: clear x.
+  ----
+  intros x p y t...
+  apply find_some in t. destruct t as [u v]. apply index_In in u. lia.
+  ----
+  intros x t... exfalso.
+  pose proof full x as u.
+  apply Exists_exists in u.
+  destruct u as [y [v w]].
+  apply (In_nth a y x) in v. destruct v as [n [q r]].
+  apply (fun z => find_none _ (index (enum A)) z (N.of_nat n, y)) in t.
+  cbn in t. unfold is_left in t. destruct (d x y) as [m | m].
+  congruence. apply m, w...
+  assert (what : (N.of_nat n, y) = nth n (index (enum A)) (N.of_nat 0, x)).
+  { unfold index. rewrite combine_nth.
+    - f_equal.
+      + rewrite map_nth. f_equal. rewrite seq_nth... lia. lia.
+      + rewrite r. reflexivity.
+    - rewrite map_length. rewrite seq_length. reflexivity. }
+  rewrite what.
+  apply nth_In.
+  rewrite index_length... lia. Qed.
+
+Equations decode_without_inspect (x : A) : {p : N | p < N.of_nat (length (enum A))} :=
+  decode_without_inspect x := (decode_fst_without_inspect x; decode_snd_without_inspect x).
+
 #[export] Instance listing_is_size_length :
   IsSize X (N.of_nat (length (enum A))).
 Proof with note.
   hnf. exists encode, decode. split.
-  - intros [p t]. apply_funelim (encode (p; t)).
-    clear p t. intros p t e e'. clear e'. exfalso. rewrite e in t. cbn in t. lia.
-    clear p t. intros p t x b e e'. clear e'.
-    apply_funelim (decode (snd (nth (N.to_nat p) (index b) (0, x)))).
-    clear x b e. intros x q y e e'. clear e'.
-    apply proj1_sig_inj. unfold proj1_sig.
-    all: admit.
-  - intros x... apply_funelim (decode x). Abort.
+  - intros [p t]. unfold encode, decode.
+    unfold enum in *. Fail induction a. Abort.
+
+#[export] Instance listing_is_size_length :
+  IsSize X (N.of_nat (length (enum A))).
+Proof with note.
+  hnf. exists encode_without_inspect, decode_without_inspect. split.
+  - intros [p t]. unfold encode_without_inspect, decode_without_inspect.
+    apply eq_sig_hprop. admit. unfold proj1_sig.
+    unfold decode_fst_without_inspect,
+    decode_fst_without_inspect_clause_1,
+    encode_without_inspect_clause_1_encode_without_inspect_fix.
+    unfold enum in *. destruct a as [| x c].
+    cbn in t; lia. Abort.
 
 #[export] Instance fin_listing_is_fin_size `{!IsFinListing X} : IsFinSize X.
 Proof.
